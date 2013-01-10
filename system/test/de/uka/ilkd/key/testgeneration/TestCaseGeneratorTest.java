ddda91490466cbdabe995156337e13e572e3f573
Christopher Svanefalk <christopher.svanefalk@gmail.com>
2013-01-10 03:51:29 +0100

    adding aux files

---
+++ system/test/de/uka/ilkd/key/testgeneration/TestCaseGeneratorTest.java
@@ -0,0 +1,32 @@
+package de.uka.ilkd.key.testgeneration;
+
+import java.io.IOException;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+
+import de.uka.ilkd.key.proof.init.ProofInputException;
+import de.uka.ilkd.key.testgeneration.keyinterface.KeYInterfaceException;
+import de.uka.ilkd.key.testgeneration.model.ModelGeneratorException;
+import de.uka.ilkd.key.testgeneration.util.Benchmark;
+import de.uka.ilkd.key.testgeneration.xml.XMLGeneratorException;
+
+public class TestCaseGeneratorTest {
+
+    @Test
+    public void test()
+            throws IOException, ProofInputException, ModelGeneratorException,
+            TestGeneratorException, KeYInterfaceException, XMLGeneratorException, InterruptedException {
+
+        TestCaseGenerator testCaseGenerator = TestCaseGenerator.getDefaultInstance();
+        String output = testCaseGenerator.generateTestCase(
+                "/home/christopher/workspace/Key/system/test/de/uka/ilkd/key/testgeneration/targetmodels/PrimitiveIntegerOperations.java",
+                "midOneProxyOneInstance");
+        
+        for(Entry<String, Long> entry : Benchmark.getReadings().entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue() + " milliseconds");
+        }
+        
+        System.out.println(output);
+    }
+}
---
+++ system/test/de/uka/ilkd/key/testgeneration/codecoverage/TestGenerator.java
@@ -0,0 +1,6 @@
+package de.uka.ilkd.key.testgeneration.codecoverage;
+
+
+public class TestGenerator {
+
+}
