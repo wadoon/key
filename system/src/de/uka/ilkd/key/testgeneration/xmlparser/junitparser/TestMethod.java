21abc2ed8ecdca047f5891d2531f88c316c002f0
Christopher Svanefalk <christopher.svanefalk@gmail.com>
2013-01-17 09:59:57 +0100

    Partial support for method parsing implemented.

--- system/src/de/uka/ilkd/key/testgeneration/xmlparser/junitparser/TestMethod.java
+++ system/src/de/uka/ilkd/key/testgeneration/xmlparser/junitparser/TestMethod.java
@@ -3,20 +3,28 @@ package de.uka.ilkd.key.testgeneration.xmlparser.junitparser;
 import java.util.LinkedList;
 import java.util.List;
 
+import de.uka.ilkd.key.java.statement.Assert;
+
 /**
- * Represents a simple view of a JUnit test method, i.e. a method annotated with @Test.
+ * This bean represents a simple view of a JUnit test method, i.e. a method annotated with @Test.
  * 
  * @author christopher
  */
 public class TestMethod {
 
     /**
-     * Object instances declared in this particular method.
+     * The name of this testMethod.
+     */
+    String name;
+
+    /**
+     * Object instances declared in this particular method, represented by {@link ObjectInstance}.
      */
     private final List<ObjectInstance> objectInstances = new LinkedList<ObjectInstance>();
 
     /**
-     * The oracle for this method
+     * The oracle for this method, ie the set of calls to methods of {@link Assert} determining
+     * whether the test case succeeded or failed.
      */
     private String oracle;
 
@@ -31,5 +39,39 @@ public class TestMethod {
     public void addObjectInstance(ObjectInstance instance) {
 
         objectInstances.add(instance);
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+
+        return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    /**
+     * @return the oracle
+     */
+    public String getOracle() {
+
+        return oracle;
+    }
+
+    /**
+     * @param oracle
+     *            the oracle to set
+     */
+    public void setOracle(String oracle) {
+
+        this.oracle = oracle;
     }
 }
