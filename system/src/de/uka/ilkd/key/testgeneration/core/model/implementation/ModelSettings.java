838912261762d787029f4a0c2e4e9d16335c36aa
Christopher Svanefalk <christopher.svanefalk@gmail.com>
2013-02-10 01:56:58 +0100

    Creating global settings for model generation subsystem

---
+++ system/src/de/uka/ilkd/key/testgeneration/core/model/implementation/ModelSettings.java
@@ -0,0 +1,148 @@
+package de.uka.ilkd.key.testgeneration.core.model.implementation;
+
+import java.io.File;
+import java.util.Collection;
+
+import de.uka.ilkd.key.gui.configuration.PathConfig;
+import de.uka.ilkd.key.gui.smt.ProofDependentSMTSettings;
+import de.uka.ilkd.key.rule.Taclet;
+import de.uka.ilkd.key.smt.SMTSettings;
+
+/**
+ * Settings for the model generation subsystem of KeYTestGen.
+ * 
+ * @author christopher
+ * 
+ */
+final class ModelSettings {
+
+    /**
+     * The number of times to re-try solving an SMT problem in the event that
+     * "UNKNOWN" is returned, since this usually seems to point to an error
+     * during the execution of the solver, rather than the problem not being
+     * solveable.
+     */
+    private static final int NUMBER_OF_TRIES = 10;
+
+    /**
+     * The default SMT settings to be used with an SMT solver used by
+     * KeYTestGen.
+     */
+    private static final DefaultSMTSettings DEFAULT_SMT_SETTINGS = new DefaultSMTSettings();
+
+    /**
+     * @return the NUMBER_OF_TRIES constant, dictating how many times a Model
+     *         Generator should re-attempt to solve an SMT problem whose last
+     *         known solution was UNKNOWN.
+     */
+    public static int getNUMBER_OF_TRIES() {
+        return NUMBER_OF_TRIES;
+    }
+
+    /**
+     * @return the DEFAULT_SMT_SETTINGS constant, containing a default set of
+     *         settings for SMT solvers used by KeYTestGen.
+     */
+    public static DefaultSMTSettings getDEFAULT_SMT_SETTINGS() {
+        return DEFAULT_SMT_SETTINGS;
+    }
+}
+
+/**
+ * Settings to pass to the SMT solver.
+ * 
+ * @author christopher
+ */
+class DefaultSMTSettings implements SMTSettings {
+
+    @Override
+    public int getMaxConcurrentProcesses() {
+
+        return 1;
+    }
+
+    @Override
+    public int getMaxNumberOfGenerics() {
+
+        return 2;
+    }
+
+    @Override
+    public String getSMTTemporaryFolder() {
+
+        return PathConfig.getKeyConfigDir() + File.separator + "smt_formula";
+    }
+
+    @Override
+    public Collection<Taclet> getTaclets() {
+
+        return null;
+    }
+
+    @Override
+    public long getTimeout() {
+
+        return 5000;
+    }
+
+    @Override
+    public boolean instantiateNullAssumption() {
+
+        return true;
+    }
+
+    @Override
+    public boolean makesUseOfTaclets() {
+
+        return false;
+    }
+
+    @Override
+    public boolean useExplicitTypeHierarchy() {
+
+        return false;
+    }
+
+    @Override
+    public boolean useBuiltInUniqueness() {
+
+        return false;
+    }
+
+    @Override
+    public boolean useAssumptionsForBigSmallIntegers() {
+
+        return false;
+    }
+
+    @Override
+    public boolean useUninterpretedMultiplicationIfNecessary() {
+
+        return false;
+    }
+
+    @Override
+    public long getMaximumInteger() {
+
+        return ProofDependentSMTSettings.getDefaultSettingsData().maxInteger;
+    }
+
+    @Override
+    public long getMinimumInteger() {
+
+        return ProofDependentSMTSettings.getDefaultSettingsData().minInteger;
+    }
+
+    @Override
+    public String getLogic() {
+
+        // return "QF_UFLIRA";
+        return "AUFLIA";
+    }
+
+    @Override
+    public boolean checkForSupport() {
+
+        return false;
+    }
+}
