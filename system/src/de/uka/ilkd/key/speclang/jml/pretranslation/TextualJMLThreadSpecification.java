76f6d25b22d1fc2dc0a14085d6f5b9b337b93c84
Daniel Bruns <bruns@kit.edu>
2014-07-28 17:25:48 +0200

    start working on JML frontend

---
+++ system/src/de/uka/ilkd/key/speclang/jml/pretranslation/TextualJMLThreadSpecification.java
@@ -0,0 +1,95 @@
+package de.uka.ilkd.key.speclang.jml.pretranslation;
+
+import de.uka.ilkd.key.collection.ImmutableList;
+import de.uka.ilkd.key.collection.ImmutableSLList;
+import de.uka.ilkd.key.speclang.PositionedString;
+
+import static de.uka.ilkd.key.util.MiscTools.equalsOrNull;
+
+public final class TextualJMLThreadSpecification extends TextualJMLConstruct {
+    
+    private ImmutableList<PositionedString> relies = ImmutableSLList.nil();
+    private ImmutableList<PositionedString> guarantees = ImmutableSLList.nil();
+    private PositionedString notAssigned;
+    private PositionedString assignable;
+
+    public TextualJMLThreadSpecification(ImmutableList<String> mods) {
+        super(ImmutableSLList.<String>nil()); // modifiers are currently ignored
+    }
+    
+    // Implementation note: append() more expensive than prepend(), but preserves original order
+
+    void addRely (PositionedString newRely) {
+        relies = relies.append(newRely);
+    }
+    
+    void addGuarantee(PositionedString newGuarantee) {
+        guarantees = guarantees.append(newGuarantee);
+    }
+    
+    void setNotAssigned(PositionedString newNotAssigned) {
+        notAssigned = newNotAssigned;
+    }
+    
+    void setAssignable(PositionedString newAssignable) {
+        assignable = newAssignable;
+    }
+
+    
+    public ImmutableList<PositionedString> getRelies() {
+        return relies;
+    }
+
+    
+    public ImmutableList<PositionedString> getGuarantees() {
+        return guarantees;
+    }
+
+    
+    public PositionedString getNotAssigned() {
+        return notAssigned;
+    }
+
+    
+    public PositionedString getAssignable() {
+        return assignable;
+    }
+    
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer();
+        sb.append("thread_behavior\n");
+        for (PositionedString rely: relies) {
+            sb.append("relies_on ");
+            sb.append(rely.toString());
+            sb.append('\n');
+        }
+        for (PositionedString guar: guarantees) {
+            sb.append("guarantees ");
+            sb.append(guar.toString());
+            sb.append('\n');
+        }
+        if (notAssigned != null) {
+            sb.append("not_assigned ");
+            sb.append(notAssigned);
+            sb.append('\n');
+        }
+        if (assignable != null) {
+            sb.append("assignable ");
+            sb.append(assignable);
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+    
+    @Override
+    public boolean equals (Object o) {
+        if (o instanceof TextualJMLThreadSpecification) {
+            final TextualJMLThreadSpecification t = (TextualJMLThreadSpecification) o;
+            return t.relies.equals(this.relies)
+                            && t.guarantees.equals(guarantees)
+                            && equalsOrNull(t.notAssigned,notAssigned)
+                            && equalsOrNull(t.assignable,assignable);
+        } else return false;
+    }
+}
