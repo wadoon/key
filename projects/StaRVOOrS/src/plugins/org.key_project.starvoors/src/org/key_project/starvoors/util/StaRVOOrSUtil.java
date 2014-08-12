ca0dc554aaee1adc4d0634df2c8d90724288b64b
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2014-08-12 10:30:50 +0200

    StaRVOOrS: Fixed failures in evaluation and added license information

--- projects/StaRVOOrS/src/plugins/org.key_project.starvoors/src/org/key_project/starvoors/util/StaRVOOrSUtil.java
+++ projects/StaRVOOrS/src/plugins/org.key_project.starvoors/src/org/key_project/starvoors/util/StaRVOOrSUtil.java
@@ -4,26 +4,28 @@ import java.io.File;
 import java.util.Set;
 
 import de.uka.ilkd.key.collection.ImmutableSet;
-import de.uka.ilkd.key.gui.configuration.ProofSettings;
 import de.uka.ilkd.key.java.abstraction.KeYJavaType;
+import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.op.IObserverFunction;
 import de.uka.ilkd.key.proof.Proof;
 import de.uka.ilkd.key.proof.SingleProof;
+import de.uka.ilkd.key.proof.init.FunctionalOperationContractPO;
 import de.uka.ilkd.key.proof.init.InitConfig;
 import de.uka.ilkd.key.proof.init.ProofInputException;
 import de.uka.ilkd.key.proof.init.ProofOblInput;
-import de.uka.ilkd.key.proof.mgt.AxiomJustification;
-import de.uka.ilkd.key.proof.mgt.ProofEnvironment;
-import de.uka.ilkd.key.proof.mgt.RuleJustification;
 import de.uka.ilkd.key.proof_references.KeYTypeUtil;
-import de.uka.ilkd.key.rule.BuiltInRule;
-import de.uka.ilkd.key.rule.OneStepSimplifier;
-import de.uka.ilkd.key.rule.RuleKey;
-import de.uka.ilkd.key.rule.Taclet;
 import de.uka.ilkd.key.speclang.Contract;
+import de.uka.ilkd.key.speclang.FunctionalOperationContract;
+import de.uka.ilkd.key.symbolic_execution.ExecutionNodePreorderIterator;
+import de.uka.ilkd.key.symbolic_execution.SymbolicExecutionTreeBuilder;
+import de.uka.ilkd.key.symbolic_execution.model.IExecutionNode;
+import de.uka.ilkd.key.symbolic_execution.model.IExecutionStart;
+import de.uka.ilkd.key.symbolic_execution.model.IExecutionTermination;
+import de.uka.ilkd.key.symbolic_execution.profile.SymbolicExecutionJavaProfile;
+import de.uka.ilkd.key.symbolic_execution.strategy.ExecutedSymbolicExecutionTreeNodesStopCondition;
 import de.uka.ilkd.key.symbolic_execution.util.KeYEnvironment;
-import de.uka.ilkd.key.ui.CustomUserInterface;
-import de.uka.ilkd.key.util.MiscTools;
+import de.uka.ilkd.key.symbolic_execution.util.SymbolicExecutionEnvironment;
+import de.uka.ilkd.key.symbolic_execution.util.SymbolicExecutionUtil;
 import de.uka.ilkd.key.util.ProofStarter;
 
 public final class StaRVOOrSUtil {
@@ -32,8 +34,7 @@ public final class StaRVOOrSUtil {
    
    public static void start(File location) throws ProofInputException {
       // Load source code and rules
-      KeYEnvironment<?> env = KeYEnvironment.load(location, null, null);
-//      KeYEnvironment<?> env = KeYEnvironment.load(SymbolicExecutionJavaProfile.getDefaultInstance(), location, null, null);
+      KeYEnvironment<?> env = KeYEnvironment.load(SymbolicExecutionJavaProfile.getDefaultInstance(), location, null, null);
       try {
          // Iterate over available types to list proof obligations
          Set<KeYJavaType> kjts = env.getJavaInfo().getAllKeYJavaTypes();
@@ -55,92 +56,65 @@ public final class StaRVOOrSUtil {
    }
 
    protected static void verify(KeYEnvironment<?> env, Contract contract) throws ProofInputException {
-System.out.println("Handing: " + contract.getName());
       InitConfig proofInitConfig = env.getInitConfig().deepCopy();
-      CustomUserInterface ui = new CustomUserInterface(false);
-      ProofOblInput proofObligation = contract.createProofObl(proofInitConfig, contract);
-      Proof proof = ui.createProof(proofInitConfig, proofObligation);
+      ProofOblInput proofObligation = new FunctionalOperationContractPO(proofInitConfig, (FunctionalOperationContract)contract, true, true);
+      Proof proof = env.getUi().createProof(proofInitConfig, proofObligation);
       try {
-//         // Configure symbolic execution settings used by the strategy of the auto mode
-//         boolean useOperationContracts = true;
-//         boolean useLoopInvarints = true;
-//         boolean nonExecutionBranchHidingSideProofs = false;
-//         boolean aliasChecks = false; // May set to true to extend path conditions for aliasing checks
-//         SymbolicExecutionEnvironment.configureProofForSymbolicExecution(proof, 
-//                                                                         ExecutedSymbolicExecutionTreeNodesStopCondition.MAXIMAL_NUMBER_OF_SET_NODES_TO_EXECUTE_PER_GOAL_IN_COMPLETE_RUN, 
-//                                                                         useOperationContracts, 
-//                                                                         useLoopInvarints, 
-//                                                                         nonExecutionBranchHidingSideProofs, 
-//                                                                         aliasChecks);
-//         // Create symbolic execution tree which contains only the start node at beginning
-//         SymbolicExecutionTreeBuilder builder = new SymbolicExecutionTreeBuilder(env.getMediator(), proof, false, false, false);
-//         builder.analyse();
-//         // Run auto mode
-//         ui.startAndWaitForAutoMode(proof);
-         OneStepSimplifier oss = MiscTools.findOneStepSimplifier(proof);
-         if (oss != null) {
-            oss.refresh(proof);
-         }
-//System.out.println("Proof: " + System.identityHashCode(oss));
-//for (RuleKey o : proof.env().getJustifInfo().rule2justif.keySet()) {
-//   if (o.r instanceof OneStepSimplifier) {
-//System.out.println("InEnv: " + System.identityHashCode(o.r));
-//   }
-//}
-
+         // Configure symbolic execution settings used by the strategy of the auto mode
+         boolean useOperationContracts = true;
+         boolean useLoopInvarints = true;
+         boolean nonExecutionBranchHidingSideProofs = false;
+         boolean aliasChecks = false; // May set to true to extend path conditions for aliasing checks
+         SymbolicExecutionEnvironment.configureProofForSymbolicExecution(proof, 
+                                                                         ExecutedSymbolicExecutionTreeNodesStopCondition.MAXIMAL_NUMBER_OF_SET_NODES_TO_EXECUTE_PER_GOAL_IN_COMPLETE_RUN, 
+                                                                         useOperationContracts, 
+                                                                         useLoopInvarints, 
+                                                                         nonExecutionBranchHidingSideProofs, 
+                                                                         aliasChecks);
+         // Create symbolic execution tree which contains only the start node at beginning
+         SymbolicExecutionTreeBuilder builder = new SymbolicExecutionTreeBuilder(env.getMediator(), proof, false, false, true);
+         builder.analyse();
+         // Run auto mode
          ProofStarter ps = new ProofStarter(false);
          ps.init(new SingleProof(proof, proofObligation.name()));
-         ps.start(true);
-         
-         oss = MiscTools.findOneStepSimplifier(proof);
-         if (oss != null) {
-            oss.refresh(proof);
-         }
-
-//         // Update symbolic execution tree
-//         builder.analyse();
-//         // Print symbolic execution tree
-//         System.out.println();
-//         System.out.println(contract.getName());
-//         System.out.println("==========================");
-//         IExecutionStart symRoot = builder.getStartNode();
-//         ExecutionNodePreorderIterator iter = new ExecutionNodePreorderIterator(symRoot);
-//         while (iter.hasNext()) {
-//            IExecutionNode next = iter.next();
-//            if (next.getChildren().length == 0) {
-//               System.out.println(next.getName() + " is " + next.getElementType());
-//            }
-//         }
+         ps.start();
+         // Update symbolic execution tree
+         builder.analyse();
+         // Analyze discovered symbolic execution tree
+         analyzeSymbolicExecutionTree(builder, contract);
       }
       finally {
-//         env.getMediator().setProof(null);
          proof.dispose();
       }
    }
-
-   private static InitConfig cloneEnvironment(KeYEnvironment<?> environment){
-      InitConfig sourceInitConfig = environment.getInitConfig();
-      // Create new InitConfig and initialize it with value from initial one.
-      InitConfig initConfig = new InitConfig(environment.getServices().copy(environment.getProfile(), false));
-      initConfig.setActivatedChoices(sourceInitConfig.getActivatedChoices());
-      ProofSettings clonedSettings = sourceInitConfig.getSettings() != null ? new ProofSettings(sourceInitConfig.getSettings()) : null;
-      initConfig.setSettings(clonedSettings);
-      initConfig.setTaclet2Builder(sourceInitConfig.getTaclet2Builder());
-      initConfig.setTaclets(sourceInitConfig.getTaclets());
-      // Create new ProofEnvironment and initialize it with values from initial one.
-      ProofEnvironment env = initConfig.getProofEnv();
-      env.setJavaModel(sourceInitConfig.getProofEnv().getJavaModel());
-      for (Taclet taclet : sourceInitConfig.activatedTaclets()) {
-         env.getJustifInfo().addJustification(taclet, sourceInitConfig.getProofEnv().getJustifInfo().getJustification(taclet));
-      }
-      for (BuiltInRule rule : initConfig.builtInRules()) {
-         RuleJustification origJusti = sourceInitConfig.getProofEnv().getJustifInfo().getJustification(rule);
-         if (origJusti == null) {
-            assert rule instanceof OneStepSimplifier;
-            origJusti = AxiomJustification.INSTANCE;
+   
+   protected static void analyzeSymbolicExecutionTree(SymbolicExecutionTreeBuilder builder, Contract contract) throws ProofInputException {
+      System.out.println();
+      System.out.println(contract.getName());
+      System.out.println("==========================");
+      IExecutionStart symRoot = builder.getStartNode();
+      ExecutionNodePreorderIterator iter = new ExecutionNodePreorderIterator(symRoot);
+      while (iter.hasNext()) {
+         IExecutionNode next = iter.next();
+         // Check if node is a leaf node
+         if (next.getChildren().length == 0) {
+            // Check if verified
+            if (next instanceof IExecutionTermination && 
+                ((IExecutionTermination)next).isBranchVerified()) {
+               // Nothing to do
+            }
+            else {
+               // Get path condition
+               Term pathCondition = next.getPathCondition();
+               // Pretty print path condition
+               String pathConditionPP = next.getFormatedPathCondition();
+               System.out.println(next.getName() + " is " + next.getElementType() + " with path condition: " + pathConditionPP);
+            }
          }
-         env.getJustifInfo().addJustification(rule, origJusti);
       }
-      return initConfig;
+   }
+   
+   public static String prettyPrint(SymbolicExecutionTreeBuilder builder, Term term) {
+      return SymbolicExecutionUtil.formatTerm(term, builder.getProof().getServices(), false, true);
    }
 }
