a127ec23048d9b54defe0ed3a5f4feadcae533db
Michael Kirsten <kirsten@ira.uka.de>
2015-01-19 15:56:49 +0100

    Allow specification elements directly in class context (w/o method context)

--- system/src/de/uka/ilkd/key/java/PrettyPrinter.java
+++ system/src/de/uka/ilkd/key/java/PrettyPrinter.java
@@ -2794,46 +2794,52 @@ public class PrettyPrinter {
        printFooter(x);
     }
     
-    protected void writeFullMethodSignature(IProgramMethod x) throws java.io.IOException {
-        write(x.getName());
-        write("(");
-        boolean afterFirst = false;
-        for (ParameterDeclaration pd : x.getParameters()) {
-            if (afterFirst) {
-                write(", ");
-            }
-            else {
-                afterFirst = true;
-            }
-            boolean originalNoLinefeed = noLinefeed;
-            try {
-               noLinefeed = true;
-               printTypeReference(pd.getTypeReference(), true);
-            }
-            finally {
-               noLinefeed = originalNoLinefeed;
-            }
-        }
-        write(")");
-    }
+	protected void writeFullMethodSignature(IProgramMethod x) throws java.io.IOException {
+		if (x == null) {
+			return;
+		} else {
+			write(x.getName());
+			write("(");
+			boolean afterFirst = false;
+			for (ParameterDeclaration pd : x.getParameters()) {
+				if (afterFirst) {
+					write(", ");
+				}
+				else {
+					afterFirst = true;
+				}
+				boolean originalNoLinefeed = noLinefeed;
+				try {
+					noLinefeed = true;
+					printTypeReference(pd.getTypeReference(), true);
+				}
+				finally {
+					noLinefeed = originalNoLinefeed;
+				}
+			}
+			write(")");
+		}
+	}
 
-    public void printExecutionContext(ExecutionContext x) 
-                    throws java.io.IOException {
-        write("source=");
-        writeFullMethodSignature(x.getMethodContext());
-        write("@");
-        writeElement(x.getTypeReference());
-        if (x.getRuntimeInstance() != null) {
-            write(",this=");
-            writeElement(x.getRuntimeInstance());
-        }
-        if (x.getThreadTypeReference() != null) {
-            write(",threadClass=");
-            writeElement(x.getThreadTypeReference());
-            write(",thread=");
-            writeElement(x.getRuntimeThreadInstance());
-        }
-    }
+	public void printExecutionContext(ExecutionContext x)
+					throws java.io.IOException {
+		write("source=");
+		writeFullMethodSignature(x.getMethodContext());
+		if (x.getMethodContext() != null) {
+			write("@");
+		}
+		writeElement(x.getTypeReference());
+		if (x.getRuntimeInstance() != null) {
+			write(",this=");
+			writeElement(x.getRuntimeInstance());
+		}
+		if (x.getThreadTypeReference() != null) {
+			write(",threadClass=");
+			writeElement(x.getThreadTypeReference());
+			write(",thread=");
+			writeElement(x.getRuntimeThreadInstance());
+		}
+	}
 
     public void printSuperConstructorReference(SuperConstructorReference x) 
 	throws java.io.IOException {
