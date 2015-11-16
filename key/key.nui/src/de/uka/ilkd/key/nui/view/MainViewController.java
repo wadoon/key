cc68bcc5be8b29a81f5278793d90b860d993a77e
Nils Muzzulini <nils@muzzulini.de>
2015-11-16 14:56:28 +0100

    resolved null pointer exception in sequent view

--- key/key.nui/src/de/uka/ilkd/key/nui/view/MainViewController.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/view/MainViewController.java
@@ -1,11 +1,6 @@
 package de.uka.ilkd.key.nui.view;
 
-import java.io.IOException;
-
 import de.uka.ilkd.key.nui.*;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 
 public class MainViewController extends ViewController {
     
