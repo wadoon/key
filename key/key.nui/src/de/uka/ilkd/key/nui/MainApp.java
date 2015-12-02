506f85342afdb817993d03854fc3b70d1ff634a6
Victor Schümmer <victor_ferdinand.schuemmer@stud.tu-darmstadt.de>
2015-12-02 14:34:44 +0100

    Layout resizing on window resize

--- key/key.nui/src/de/uka/ilkd/key/nui/MainApp.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/MainApp.java
@@ -10,6 +10,8 @@ import org.reflections.Reflections;
 import de.uka.ilkd.key.nui.view.RootLayoutController;
 import de.uka.ilkd.key.proof.Proof;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -29,7 +31,8 @@ public class MainApp extends Application {
         this.primaryStage.setTitle("KeY Project");
 
         // Set the application icon.
-        this.primaryStage.getIcons().add(new Image("file:resources/images/key-color-icon-square.png"));
+        this.primaryStage.getIcons().add(
+                new Image("file:resources/images/key-color-icon-square.png"));
 
         initRootLayout();
         scanForViews();
@@ -44,49 +47,71 @@ public class MainApp extends Application {
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            URL path= MainApp.class.getResource("view/RootLayout.fxml");
-            if(path == null)
+            URL path = MainApp.class.getResource("view/RootLayout.fxml");
+            if (path == null)
                 throw new RuntimeException("Could not find RootLayout.fxml");
             loader.setLocation(path);
             rootLayout = (BorderPane) loader.load();
 
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
+            scene.widthProperty().addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(
+                        ObservableValue<? extends Number> observableValue,
+                        Number oldSceneWidth, Number newSceneWidth) {
+                    ((RootLayoutController) loader.getController()).resize();
+                }
+            });
+            scene.heightProperty().addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(
+                        ObservableValue<? extends Number> observableValue,
+                        Number oldSceneHeight, Number newSceneHeight) {
+                    ((RootLayoutController) loader.getController()).resize();
+                }
+            });
             primaryStage.setScene(scene);
 
             // Give the controller access to the main app.
             RootLayoutController controller = loader.getController();
             controller.setMainApp(this);
             rootLayoutController = controller;
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void scanForViews() {
-        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(KeYView.class);
+        Set<Class<?>> annotated = reflections
+                .getTypesAnnotatedWith(KeYView.class);
         for (Class<?> c : annotated) {
             KeYView annot = c.getAnnotation(KeYView.class);
             // no used yet
-            //if (Arrays.asList(annot.windows()).contains("Main"))
-            rootLayoutController.registerView(annot.title(), c.getResource(annot.path()),
-                    annot.preferredPosition(),annot.accelerator());
+            // if (Arrays.asList(annot.windows()).contains("Main"))
+            rootLayoutController.registerView(annot.title(),
+                    c.getResource(annot.path()), annot.preferredPosition(),
+                    annot.accelerator());
         }
         System.out.println("Views: " + annotated.size());
     }
 
     private void scanForMenus() {
-        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(KeYMenu.class);
+        Set<Class<?>> annotated = reflections
+                .getTypesAnnotatedWith(KeYMenu.class);
         for (Class<?> c : annotated) {
             KeYMenu annot = c.getAnnotation(KeYMenu.class);
             // not used yet
-            //if (Arrays.asList(annot.windows()).contains("Main")) {
-            if (annot.parentMenu().equals("")){
+            // if (Arrays.asList(annot.windows()).contains("Main")) {
+            if (annot.parentMenu().equals("")) {
                 rootLayoutController.registerMenu(c.getResource(annot.path()));
-            } else {
-                rootLayoutController.registerMenuEntry(c.getResource(annot.path()), annot.parentMenu());
             }
-            //}
+            else {
+                rootLayoutController.registerMenuEntry(
+                        c.getResource(annot.path()), annot.parentMenu());
+            }
+            // }
         }
         System.out.println("Menus: " + annotated.size());
     }
