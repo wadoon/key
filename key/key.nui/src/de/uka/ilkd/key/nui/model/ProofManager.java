b9dff4b32169b63b8b71aa543f988ca4d78537ef
Benedikt Gross <benedikt.gross@gmx.de>
2015-12-17 21:50:03 +0100

    first try on KeYMediator

--- key/key.nui/src/de/uka/ilkd/key/nui/model/ProofManager.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/model/ProofManager.java
@@ -2,29 +2,39 @@ package de.uka.ilkd.key.nui.model;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.EventObject;
 import java.util.Iterator;
 import java.util.List;
 
+import de.uka.ilkd.key.control.AutoModeListener;
 import de.uka.ilkd.key.control.KeYEnvironment;
-import de.uka.ilkd.key.nui.Context;
-import de.uka.ilkd.key.nui.MainApp;
+import de.uka.ilkd.key.core.KeYMediator;
+import de.uka.ilkd.key.core.KeYSelectionEvent;
+import de.uka.ilkd.key.core.KeYSelectionListener;
+import de.uka.ilkd.key.nui.MediatorUserInterface;
+import de.uka.ilkd.key.nui.model.ProofManager.DummyProofListener;
 import de.uka.ilkd.key.nui.util.IStatusManager;
 import de.uka.ilkd.key.proof.Proof;
 import de.uka.ilkd.key.proof.init.JavaProfile;
 import de.uka.ilkd.key.proof.io.ProblemLoaderException;
+import de.uka.ilkd.key.settings.SettingsListener;
 
 public class ProofManager {
-
     private Proof proof;
-    private IStatusManager status;
+    private IStatusManager statusManager;
     private List<IProofListener> listeners = new ArrayList<IProofListener>();
+    private KeYMediator mediator;
 
     /**
      * Creates a new Proofmanager
      * @param status a StatusManager to print status texts to
      */
-    public ProofManager(IStatusManager status) {
-        this.status = status;
+    public ProofManager(IStatusManager statusManager) {
+        this.statusManager = statusManager;
+        MediatorUserInterface userInterface = new MediatorUserInterface(statusManager);
+        mediator = new KeYMediator(userInterface);
+        userInterface.setMediator(mediator);
+        mediator.addKeYSelectionListener(new DummyProofListener());
     }
 
     public synchronized void addProofListener(IProofListener proofListener) {
@@ -58,36 +68,52 @@ public class ProofManager {
      * @param file
      *            Proof to be loaded.
      */
-    public synchronized void setProof(File proofFile) {
+    public synchronized void loadProblem(File proofFile) {
         // this.proof.setProofFile(proofFile);
-        status.setStatus("Loading Proof...");
-        this.proof = loadProof(proofFile);
-        status.setStatus("Proof loaded: " + proofFile.getName());
-        fireProofUpdatedEvent();
+        statusManager.setStatus("Loading Proof...");
+        mediator.getUI().loadProblem(proofFile);
+     //   statusManager.setStatus("Proof loaded: " + proofFile.getName());
+       // fireProofUpdatedEvent();
     }
 
-    /**
-     * Loads the given proof file. Checks if the proof file exists and the proof
-     * is not null, and fails if the proof could not be loaded.
-     *
-     * @param proofFileName
-     *            The file name of the proof file to load.
-     * @return The loaded proof.
-     */
-    private Proof loadProof(File proofFile) {
-        // File proofFile = new File("../" + proofFileName);
+//    /**
+//     * Loads the given proof file. Checks if the proof file exists and the proof
+//     * is not null, and fails if the proof could not be loaded.
+//     *
+//     * @param proofFileName
+//     *            The file name of the proof file to load.
+//     * @return The loaded proof.
+//     */
+//    private Proof loadProof(File proofFile) {
+//        // File proofFile = new File("../" + proofFileName);
+//        
+//        try {
+//            KeYEnvironment<?> environment = KeYEnvironment.load(
+//                    JavaProfile.getDefaultInstance(), proofFile, null, null,
+//                    null, true);
+//            Proof proof = environment.getLoadedProof();
+//
+//            return proof;
+//        }
+//        catch (ProblemLoaderException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+//    
 
-        try {
-            KeYEnvironment<?> environment = KeYEnvironment.load(
-                    JavaProfile.getDefaultInstance(), proofFile, null, null,
-                    null, true);
-            Proof proof = environment.getLoadedProof();
-
-            return proof;
+    class DummyProofListener implements  KeYSelectionListener {
+        @Override
+        public void selectedNodeChanged(KeYSelectionEvent e) {
+            if (mediator.isInAutoMode()) {
+                return;
+            }
         }
-        catch (ProblemLoaderException e) {
-            e.printStackTrace();
-            return null;
+
+        @Override
+        public void selectedProofChanged(KeYSelectionEvent e) {
+            proof = e.getSource().getSelectedProof();
+            fireProofUpdatedEvent();
         }
     }
 }
