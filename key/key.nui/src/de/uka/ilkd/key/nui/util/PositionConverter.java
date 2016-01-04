1af23e3cfe87f88a353f407911421281c46b727f
Maximilian Li <maximilian.xiling.li@gmail.com>
2016-01-04 15:40:51 +0100

    Code Cleaned. GetInt for Char added.

--- key/key.nui/src/de/uka/ilkd/key/nui/util/PositionConverter.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/util/PositionConverter.java
@@ -17,16 +17,50 @@ public class PositionConverter {
      * 
      */
     public PositionConverter(String proofString) {
-        proofString = proofString;
+        this.proofString = proofString;
         strings = proofString.split("\n");
     }
 
+    /**
+     * takes a MouseEvent and computes proofChar with MouseEvent Position
+     * 
+     * @param event
+     */
     public void takeMouseEvent(MouseEvent event) {
-        int row = (int) Math.round((event.getY() -15.0)/ 18.0);
-        int charInRow = (int) Math.round((event.getX()- 10) /9.6);
-        if (row >= 0 && row < strings.length && charInRow >= 0 && charInRow < strings[row].length()) {
-            System.out.println(event.getY() + ", " + event.getX() + ": " + strings[row].charAt(charInRow));
+        int row = (int) Math.round((event.getY() - 15.0) / 18.0);
+        int charInRow = (int) Math.round((event.getX() - 10) / 9.6);
+
+        if (row >= 0 && row < strings.length && charInRow >= 0
+                && charInRow < strings[row].length()) {
+
+            int idx = getCharIndex(row, charInRow);
+            char c = strings[row].charAt(charInRow);
+
+            System.out.println("Index: " + idx);
+            System.out.println("Char at this Pos: " + proofString.charAt(idx));
+            System.out.println(event.getY() + ", " + event.getX() + ": " + c);
+
         }
+        System.out.println("####");
+    }
+
+    /**
+     * gets the Index of a specific Char in the proofString
+     * 
+     * @param row
+     *            the row of the char
+     * @param charInRow
+     *            the position of the char inside of the row
+     * @return returns the index of the char in proofstring
+     */
+    private int getCharIndex(int row, int charInRow) {
+        int idx = 0;
+        for (int i = 0; i < row; i++) {
+            idx += strings[i].length();
+        }
+        idx += charInRow + row;
+
+        return idx;
     }
 
 }
