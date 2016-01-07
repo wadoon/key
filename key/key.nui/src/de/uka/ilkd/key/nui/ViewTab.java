3736803be8ed3dd1e8a390231dbaaef96c2fa88f
Benedikt Gross <benedikt.gross@gmx.de>
2016-01-07 19:20:05 +0100

    removed dirt from merge

--- key/key.nui/src/de/uka/ilkd/key/nui/ViewTab.java
+++
@@ -1,9 +0,0 @@
-package de.uka.ilkd.key.nui;
-
-import de.uka.ilkd.key.nui.model.ViewInformation;
-import javafx.scene.control.Tab;
-
-public class ViewTab {
-    public ViewInformation information;
-    public Tab tab;
-}
