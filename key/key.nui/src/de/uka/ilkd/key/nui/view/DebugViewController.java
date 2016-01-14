d00180f752cec088ece0a199c8e52c463f652628
Benedikt Gross <benedikt.gross@gmx.de>
2016-01-14 21:35:25 +0100

    fixed debug view

--- key/key.nui/src/de/uka/ilkd/key/nui/view/DebugViewController.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/view/DebugViewController.java
@@ -7,26 +7,26 @@ import de.uka.ilkd.key.nui.KeYView;
 import de.uka.ilkd.key.nui.ViewController;
 import de.uka.ilkd.key.nui.ViewPosition;
 import javafx.fxml.FXML;
-import javafx.print.PrintColor;
 import javafx.scene.control.TextArea;
 
-@KeYView(title = "Debug", path = "", preferredPosition = ViewPosition.TOPRIGHT)
+@KeYView(title = "Debug", path = "DebugView.fxml", preferredPosition = ViewPosition.TOPRIGHT)
 public class DebugViewController extends ViewController {
 
     @FXML
     private TextArea outputText;
-    
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         debugViewController = this;
     }
-    
-    public void Print(String str){
+
+    public void Print(String str) {
         outputText.setText(str);
     }
-    
+
     private static DebugViewController debugViewController;
-    public static void PrintOnCurrent(String str){
+
+    public static void PrintOnCurrent(String str) {
         debugViewController.Print(str);
     }
 }
