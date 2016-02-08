4dd0ddd7f386987b1adb4febd99bbc52ecb3dfbe
Victor Schümmer <victor_ferdinand.schuemmer@stud.tu-darmstadt.de>
2016-02-08 15:41:20 +0100

    Basic css reader/writer/parser

---
+++ key/key.nui/src/de/uka/ilkd/key/nui/util/CssFileHandler.java
@@ -0,0 +1,171 @@
+package de.uka.ilkd.key.nui.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.key_project.util.java.IOUtil;
+
+public class CssFileHandler {
+    
+    private ArrayList<CssRule> parsedRules;
+    private String css;
+    
+    private enum State {
+        COMMENT, SELECTOR, PROPERTY, VALUE;
+    }
+
+    /**
+     * Constructs a CssFileHandler without file.
+     * Call loadCssFile to add the file afterwards.
+     */
+    public CssFileHandler() {
+        css = "";
+        parsedRules = new ArrayList<CssRule>();
+    }
+
+    /**
+     * Constructs a CssFileHandler.
+     * @param url path to the css file
+     * @throws IOException
+     */
+    public CssFileHandler(URL url) throws IOException {
+        this();
+        loadCssFile(url);
+    }
+
+    /**
+     * Loads a css file.
+     * @param url path to the css file
+     * @throws IOException
+     */
+    public void loadCssFile(URL url) throws IOException {
+        css = IOUtil.readFrom(url);
+    }
+
+    /**
+     * Writes to css file
+     * @param url path to the css file
+     * @throws IOException
+     */
+    public void writeCssFile(URL url) throws IOException {
+        File file = new File(IOUtil.toURI(url));
+        FileOutputStream fop = new FileOutputStream(file);
+        IOUtil.writeTo(fop, css);
+    }
+
+    /**
+     * @return the css content string
+     */
+    public String getCss() {
+        return css;
+    }
+    
+    /**
+     * Returns a List of CssRules parsed from the css file. 
+     * List will be empty if no file was loaded.
+     * @return List of CssRules 
+     */
+    public List<CssRule> getParsedRules() {
+        return parsedRules;
+    }
+
+    private String removeSpacing(String str) {
+        return str.replace("\n", "").replace("\r", "").replace("\t", "")
+                .replace(" ", "");
+    }
+
+    /**
+     * Parses the loaded css and returns the rules.
+     * @return List of CssRules
+     */
+    public List<CssRule> parse() {
+        CssRule rule = new CssRule();
+        String selector = "";
+        String property = "";
+        String value = "";
+        State state = State.SELECTOR;
+        String css = removeSpacing(this.css);
+        
+        for (int i = 0; i < css.length() - 1; i++) {
+            char c = css.charAt(i);
+            if (c == '/' && css.charAt(i + 1) == '*') {
+                // state = State.COMMENT;
+            }
+
+            switch (state) {
+            case COMMENT: {
+                // TODO implement comment handle
+                break;
+            }
+            case SELECTOR: {
+                switch (c) {
+                case '{': {
+                    state = State.PROPERTY;
+                    rule.addSelector(selector);
+                    selector = "";
+                    break;
+                }
+                case ',': {
+                    // TODO catch leading comma
+                    // if (selector.equals(""))
+                    rule.addSelector(selector);
+                    selector = "";
+                }
+                default:
+                    selector += c;
+                }
+                break;
+            }
+            case PROPERTY: {
+                switch (c) {
+                case ':': {
+                    state = State.VALUE;
+                    break;
+                }
+                case ';': {
+                    // TODO catch
+                    break;
+                }
+                case '}': {
+
+                    parsedRules.add(rule);
+                    rule = new CssRule();
+                    state = State.SELECTOR;
+                    break;
+                }
+                default:
+                    property += c;
+                }
+                break;
+            }
+            case VALUE: {
+                switch (c) {
+                case ';': {
+                    rule.addPropertyValuePair(property, value);
+                    property = "";
+                    value = "";
+                    state = State.PROPERTY;
+                    break;
+                }
+                case ',': {
+                    // TODO catch
+                    break;
+                }
+                case '}': {
+                    // TODO catch
+                    break;
+                }
+                default:
+                    value += c;
+                }
+                break;
+            }
+            }
+        }
+        return parsedRules;
+    }
+}
