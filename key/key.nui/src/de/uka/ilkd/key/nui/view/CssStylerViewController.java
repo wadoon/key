e41f09cb6659489f1061e235e514a0b7633aec0e
Maximilian Li <maximilian.xiling.li@gmail.com>
2016-02-25 23:21:30 +0100

    property specific controls added

--- key/key.nui/src/de/uka/ilkd/key/nui/view/CssStylerViewController.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/view/CssStylerViewController.java
@@ -4,6 +4,7 @@
 package de.uka.ilkd.key.nui.view;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -12,21 +13,32 @@ import java.util.ResourceBundle;
 import de.uka.ilkd.key.nui.ViewController;
 import de.uka.ilkd.key.nui.util.CssFileHandler;
 import de.uka.ilkd.key.nui.util.CssRule;
+import de.uka.ilkd.key.nui.util.NUIConstants;
 import de.uka.ilkd.key.nui.util.PreviewPrinter;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.util.Callback;
@@ -42,21 +54,26 @@ public class CssStylerViewController extends ViewController {
     private String selected;
     private CssFileHandler cssFileHandler;
     private PreviewPrinter previewPrinter = new PreviewPrinter();
+    private HashMap<String, String> masterRules;
+
+    private ObservableList<String> fontWeight = FXCollections
+            .observableArrayList("normal", "bold");
+    private ObservableList<String> fontStyle = FXCollections
+            .observableArrayList("normal", "italic");
+    private ObservableList<String> fontFamily = FXCollections
+            .observableArrayList("Arial", "Liberation Mono", "Courier New",
+                    "Comic Sans", "Times New Roman");
 
     @FXML
     private ListView<String> listView;
-    @FXML
-    private TableColumn<Map.Entry<String, String>, String> propertyColumn;
-    @FXML
-    private TableColumn<Map.Entry<String, String>, String> valueColumn;
-    @FXML
-    private TableView<Map<String, String>> table;
     @FXML
     private Button apply;
     @FXML
     private Button reset;
     @FXML
     private WebView previewWeb;
+    @FXML
+    private GridPane propValGrid;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -68,47 +85,14 @@ public class CssStylerViewController extends ViewController {
         cssFileHandler = getContext().getCssFileHandler();
 
         initializeList();
-
-        propertyColumn.setCellValueFactory(
-                //TODO Findbugs wants this to be refactored into a named static inner class. discuss with team
-                new Callback<TableColumn.CellDataFeatures<Map.Entry<String, String>, String>, ObservableValue<String>>() {
-
-                    @Override
-                    public ObservableValue<String> call(
-                            TableColumn.CellDataFeatures<Map.Entry<String, String>, String> p) {
-                        // this callback returns property for just one cell, you
-                        // can't use a loop here
-                        // for first column we use key
-                        return new SimpleStringProperty(p.getValue().getKey());
-                    }
-                });
-        valueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-        valueColumn.setCellValueFactory(
-                //TODO Findbugs wants this to be refactored into a named static inner class. discuss with team
-                new Callback<TableColumn.CellDataFeatures<Map.Entry<String, String>, String>, ObservableValue<String>>() {
-
-                    @Override
-                    public ObservableValue<String> call(
-                            TableColumn.CellDataFeatures<Map.Entry<String, String>, String> p) {
-                        // this callback returns property for just one cell, you
-                        // can't use a loop here
-                        // for first column we use key
-                        return new SimpleStringProperty(
-                                p.getValue().getValue());
-                    }
-                });
-        valueColumn.setOnEditCommit(e -> {
-            ruleMap.get(selected).getPropertyValuePairs()
-                    .put(e.getRowValue().getKey(), e.getNewValue());
-            apply.setDisable(false);
-            reset.setDisable(false);
-            this.updatePreview();
-        });
     }
 
     private void initializeList() {
         for (CssRule rule : cssFileHandler.getParsedRules()) {
             ruleMap.put(rule.selectorsAsString(), rule);
+            if (rule.selectorsAsString().equals("pre")) {
+                masterRules = rule.getPropertyValuePairs();
+            }
         }
 
         ObservableList<String> ruleList = FXCollections
@@ -123,14 +107,147 @@ public class CssStylerViewController extends ViewController {
     }
 
     private void updateTable() {
-        table.setItems(FXCollections.observableArrayList(
-                ((Map) ruleMap.get(selected).getPropertyValuePairs())
-                        .entrySet()));
+        propValGrid.getChildren().clear();
+
+        HashMap<String, String> propertyValuePairMap = ruleMap.get(selected)
+                .getPropertyValuePairs();
+        int i = 0;
+        for (String property : propertyValuePairMap.keySet()) {
+            String value = propertyValuePairMap.get(property);
+            String propertyLabel;
+            Node valueNode;
+            boolean inherited = false;
+
+            if (value.equals("inherit")) {
+                value = masterRules.get(property);
+                inherited = true;
+            }
+
+            switch (property) {
+            case "background-color":
+                propertyLabel = "Background Color:";
+                valueNode = new ColorPicker(convertToColor(value));
+                ((ColorPicker) valueNode).setOnAction(event -> {
+                    Color c = ((ColorPicker) valueNode).getValue();
+
+                    propertyValuePairMap.put(property,
+                            String.format("#%02X%02X%02X",
+                                    (int) (c.getRed() * 255),
+                                    (int) (c.getGreen() * 255),
+                                    (int) (c.getBlue() * 255)));
+                    updatePreview();
+                });
+                break;
+            case "color":
+                propertyLabel = "Font Color:";
+                valueNode = new ColorPicker(convertToColor(value));
+                ((ColorPicker) valueNode).setOnAction(event -> {
+                    Color c = ((ColorPicker) valueNode).getValue();
+
+                    propertyValuePairMap.put(property,
+                            String.format("#%02X%02X%02X",
+                                    (int) (c.getRed() * 255),
+                                    (int) (c.getGreen() * 255),
+                                    (int) (c.getBlue() * 255)));
+                    updatePreview();
+                });
+                break;
+            case "font-weight":
+                propertyLabel = "Boldness:";
+                valueNode = new ComboBox<>(fontWeight);
+                ((ComboBox<String>) valueNode).setValue(value);
+                ((ComboBox<String>) valueNode).setOnAction(event -> {
+                    propertyValuePairMap.put(property,
+                            ((ComboBox<String>) valueNode).getValue());
+                    updatePreview();
+                });
+                break;
+            case "font-size":
+                propertyLabel = "Font Size in px:";
+                valueNode = new TextField(
+                        value.substring(0, value.length() - 2));
+                break;
+            case "font-family":
+                propertyLabel = "Font:";
+                valueNode = new ComboBox<>(fontFamily);
+                ((ComboBox<String>) valueNode).setValue(value);
+                ((ComboBox<String>) valueNode).setOnAction(event -> {
+                    propertyValuePairMap.put(property,
+                            ((ComboBox<String>) valueNode).getValue());
+                    updatePreview();
+                });
+                break;
+            case "font-style":
+                propertyLabel = "Italics:";
+                valueNode = new ComboBox<>(fontStyle);
+                ((ComboBox<String>) valueNode).setValue(value);
+                ((ComboBox<String>) valueNode).setOnAction(event -> {
+                    propertyValuePairMap.put(property,
+                            ((ComboBox<String>) valueNode).getValue());
+                    updatePreview();
+                });
+                break;
+            case "display":
+                propertyLabel = "Do not edit this rule!";
+                valueNode = new Label();
+                break;
+            default:
+                propertyLabel = property;
+                valueNode = new TextField(value);
+                break;
+            }
+
+            CheckBox cbxInherited = new CheckBox("Inherited");
+            cbxInherited.setSelected(false);
+            cbxInherited.setOnAction(event -> {
+                if (cbxInherited.isSelected()) {
+                    if (valueNode instanceof ColorPicker) {
+                        ((ColorPicker) valueNode).setValue(
+                                convertToColor(masterRules.get(property)));
+                    }
+                    else if (valueNode instanceof ComboBox<?>) {
+                        ((ComboBox<String>) valueNode)
+                                .setValue(masterRules.get(property));
+                    }
+                    else {
+                        TextField tf = (TextField) valueNode;
+                        String text = masterRules.get(property);
+                        if (property.equals("font-size")) {
+                            text = text.substring(0, text.length() - 2);
+                        }
+                        tf.setText(text);
+                    }
+                    propertyValuePairMap.put(property,
+                            masterRules.get(property));
+                    updatePreview();
+                }
+                valueNode.setDisable(cbxInherited.isSelected());
+            });
+            if (inherited) {
+                valueNode.setDisable(true);
+                cbxInherited.setSelected(true);
+            }
+            propValGrid.add(new Label(propertyLabel), 0, i);
+            propValGrid.add(valueNode, 1, i);
+            if (!(selected.equals("pre") || selected
+                    .equals("." + NUIConstants.FILTER_COLLAPSED_TAG))) {
+                propValGrid.add(cbxInherited, 2, i);
+            }
+            i++;
+        }
+    }
+
+    private Color convertToColor(String colorStr) {
+        return new Color(Integer.valueOf(colorStr.substring(1, 3), 16) / 255,
+                Integer.valueOf(colorStr.substring(3, 5), 16) / 255,
+                Integer.valueOf(colorStr.substring(5, 7), 16) / 255, 1.0);
     }
 
     private void updatePreview() {
         previewWeb.getEngine().loadContent(previewPrinter
                 .printPreview(cssFileHandler.parsedRulestoString(), selected));
+        apply.setDisable(false);
+        reset.setDisable(false);
     }
 
     public void setStage(Stage stage) {
@@ -181,7 +298,7 @@ public class CssStylerViewController extends ViewController {
         apply.setDisable(true);
         reset.setDisable(true);
     }
-
+    //XXX BUG when calling Reset();
     @FXML
     private void handleReset() {
         cssFileHandler.reset();
@@ -189,7 +306,7 @@ public class CssStylerViewController extends ViewController {
 
         updateTable();
         updatePreview();
-        
+
         apply.setDisable(true);
         reset.setDisable(true);
     }
