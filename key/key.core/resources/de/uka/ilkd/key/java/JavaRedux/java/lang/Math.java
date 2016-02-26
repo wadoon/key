d769f48343451fee3a5b6f001deff9f71a50dd9f
Michael Kirsten <kirsten@ira.uka.de>
2016-02-26 11:28:22 +0100

    Make all necessary Java types available for recoder

---
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/java/lang/Math.java
@@ -0,0 +1,9 @@
+package java.lang;
+
+/**
+ * Exists only for proving MixServer of sElect
+ */
+public final class Math extends java.lang.Object
+{
+	static int min(int a, int b);
+}
---
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/java/nio/charset/Charset.java
@@ -0,0 +1,9 @@
+package java.nio.charset;
+
+/**
+ * Exists only for proving MixServer of sElect
+ */
+public abstract class Charset extends java.lang.Object implements java.lang.Comparable
+{
+
+}
