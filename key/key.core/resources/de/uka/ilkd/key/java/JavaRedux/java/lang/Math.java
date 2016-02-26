aa04470b1bd03737890992a382f2c2686455ff2b
Mihai Herda <herda@kit.edu>
2016-02-26 12:14:11 +0100

    Added missing types.

---
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/java/lang/Math.java
@@ -0,0 +1,10 @@
+/* This file is needed for the e-voting case study.
+ * 
+ */
+package java.lang;
+
+public final class Math extends java.lang.Object
+{
+     static int min(int a, int b);
+   
+}
---
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/java/nio/charset/Charset.java
@@ -0,0 +1,9 @@
+package java.nio.charset;
+
+
+public abstract class Charset extends Object implements Comparable
+{
+
+public static Charset forName(String charsetName);
+
+}
