c2db42d4f96c75a2d8b8ffc1427d49ca847dc1ec
Victor Schümmer <victor_ferdinand.schuemmer@stud.tu-darmstadt.de>
2016-02-29 20:53:22 +0100

    Revert "fixed performance of loadFxml()"
    This reverts commit 126f609a34ff9c94c002cfbbe99777209146ec78.

--- key/key.nui/src/de/uka/ilkd/key/nui/util/KeyFxmlLoader.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/util/KeyFxmlLoader.java
@@ -2,33 +2,21 @@ package de.uka.ilkd.key.nui.util;
 
 import java.io.IOException;
 import java.net.URL;
-
-import de.uka.ilkd.key.java.declaration.modifier.Public;
-import de.uka.ilkd.key.java.reference.ThisConstructorReference;
 import javafx.fxml.FXMLLoader;
 import javafx.util.Pair;
 
-public class KeyFxmlLoader {
-
-    private static FXMLLoader loader;
-
-    public static FXMLLoader getLoaderInstance() {
-        if (loader == null)
-            loader = new FXMLLoader();
-        return loader;
-    }
-
-    public static <T, C> Pair<T, C> loadFxml(URL path) {
+public class KeyFxmlLoader {    
+    public static <T,C> Pair<T, C> loadFxml(URL path) {
         try {
             // Load main view
-            FXMLLoader loader = getLoaderInstance();
+            FXMLLoader loader = new FXMLLoader();
             loader.setLocation(path);
 
             T content = loader.load();
 
             // Give the controller access to the main app.
             C controller = loader.getController();
-            return new Pair<T, C>(content, controller);
+            return new Pair<T,C>(content,controller);
         }
         catch (IOException e) {
             e.printStackTrace();
