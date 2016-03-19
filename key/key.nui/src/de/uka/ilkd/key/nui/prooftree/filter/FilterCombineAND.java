707b83f0bd797701208a9425fd69e1a53ec9f0d5
Stefan Pilot <hans.stefan.pilot@googlemail.com>
2016-03-19 02:31:01 +0100

    fixes some PMD Warnings in prooftree/filter/FilterCombineAND.java

--- key/key.nui/src/de/uka/ilkd/key/nui/prooftree/filter/FilterCombineAND.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/prooftree/filter/FilterCombineAND.java
@@ -13,33 +13,47 @@ public class FilterCombineAND implements ProofTreeFilter {
     /**
      * The first filter used for combination.
      */
-    ProofTreeFilter f1;
-
+    private final ProofTreeFilter filter1;
     /**
      * The second filter used for combination.
      */
-    ProofTreeFilter f2;
-
+    private final ProofTreeFilter filter2;
     /**
      * Constructor.
      * 
-     * @param f1
+     * @param filter1
      *            The first filter.
-     * @param f2
+     * @param filter2
      *            The second filter.
      */
-    public FilterCombineAND(ProofTreeFilter f1, ProofTreeFilter f2) {
-        this.f1 = f1;
-        this.f2 = f2;
-    }
-
-    @Override
-    public boolean test(final NUINode node) {
-        return f1.test(node) && f2.test(node);
+    public FilterCombineAND(final ProofTreeFilter filter1, final ProofTreeFilter filter2) {
+        this.filter1 = filter1;
+        this.filter2 = filter2;
     }
 
     @Override
     public String getContextMenuItemText() {
         return "";
+    }
+
+    /**
+     * TODO
+     * @return
+     */
+    public ProofTreeFilter getFilter1() {
+        return filter1;
+    }
+
+    /**
+     * TODO
+     * @return
+     */
+    public ProofTreeFilter getFilter2() {
+        return filter2;
+    }
+
+    @Override
+    public boolean test(final NUINode node) {
+        return filter1.test(node) && filter2.test(node);
     }
 }
