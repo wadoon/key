b32f47964dea9a98b2d7217ebe86e0697a6484e4
Benedikt Gross <benedikt.gross@gmx.de>
2016-03-20 17:36:04 +0100

    mouse icon in filter

--- key/key.nui/src/de/uka/ilkd/key/nui/util/NUIConstants.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/util/NUIConstants.java
@@ -35,6 +35,7 @@ public final class NUIConstants {
 
     public final static String DEFAULT_XML_PATH = "resources/xml/cssList.xml";
     public final static String KEY_WINDOW_ICON = "file:resources/images/key-color-icon-square.png";
+    public final static String FILTER_MOUSE_ICON = "file:resources/images/mouse_icon.png";
 
     public final static String DEFAULT_SEQUENT_CSS = "pre{background-color:#FFFFFF;color:#000000;font-weight:normal;font-size:16px;font-family:Courier New;font-style:normal;}"
             + ".highlighted{background-color:#FFFF00;color:inherit;font-weight:inherit;font-style:inherit;}"
