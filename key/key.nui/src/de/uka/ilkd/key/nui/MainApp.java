3ee83d54a28572ed36fddc07bb37723a8cb38f60
Benedikt Gross <benedikt.gross@gmx.de>
2016-03-31 21:57:52 +0200

    removed Platform.exit

--- key/key.nui/src/de/uka/ilkd/key/nui/MainApp.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/MainApp.java
@@ -385,7 +385,6 @@ public class MainApp extends Application {
                 .store(PathConfig.getRecentFileStorage());
         System.out.println("Where we go from here is a choice I leave to you.");
         primaryStage.close();
-        Platform.exit();
     }
 
     /**
