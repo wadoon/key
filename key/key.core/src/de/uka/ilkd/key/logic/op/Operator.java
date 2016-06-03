e13f2bd2d8a0350452521c31f101fa5cd3bf5af8
Dominic Scheurer <scheurer@cs.tu-darmstadt.de>
2016-06-03 12:20:02 +0200

    [Refactoring] Extracted generic operator interface

--- key/key.core/src/de/uka/ilkd/key/logic/op/Operator.java
+++ key/key.core/src/de/uka/ilkd/key/logic/op/Operator.java
@@ -19,51 +19,35 @@ import de.uka.ilkd.key.logic.Named;
 import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.sort.Sort;
 
-
-/** 
- * All symbols acting as members of a term e.g. logical operators, predicates, 
- * functions, variables etc. have to implement this interface.  
+/**
+ * Provides methods used in type checking.<br/>
+ * 
+ * <strong>TODO:</strong> This should be named something like
+ * "TypeCheckingAndInferenceService" and not be used for assembling actual
+ * terms.
  */
-public interface Operator extends Named, SVSubstitute {
-    
-    /**
-     * the arity of this operator  
-     */
-    int arity();
-    
+public interface Operator extends Named, SVSubstitute, GenericOperator {
 
     /**
      * Determines the sort of the {@link Term} if it would be created using this
      * Operator as top level operator and the given terms as sub terms. The
      * assumption that the constructed term would be allowed is not checked.
-     * @param terms an array of Term containing the subterms of a (potential)
-     * term with this operator as top level operator
+     * 
+     * @param terms
+     *            an array of Term containing the subterms of a (potential) term
+     *            with this operator as top level operator
      * @return sort of the term with this operator as top level operator of the
-     * given substerms
+     *         given substerms
      */
     Sort sort(ImmutableArray<Term> terms);
-    
-    
+
     /**
-     * Tells whether the operator binds variables at the n-th subterm.
-     */
-    boolean bindVarsAt(int n);
-    
-    
-    /**
-     * Tells whether the operator is rigid.
-     */
-    boolean isRigid();      
-    
-    
-    /**
-     * Checks whether the top level structure of the given @link Term
-     * is syntactically valid, given the assumption that the top level
-     * operator of the term is the same as this Operator. The
-     * assumption that the top level operator and the term are equal
-     * is NOT checked.  
-     * @return true iff the top level structure of
-     * the {@link Term} is valid.
+     * Checks whether the top level structure of the given @link Term is
+     * syntactically valid, given the assumption that the top level operator of
+     * the term is the same as this Operator. The assumption that the top level
+     * operator and the term are equal is NOT checked.
+     * 
+     * @return true iff the top level structure of the {@link Term} is valid.
      */
     boolean validTopLevel(Term term);
 }
