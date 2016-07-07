c762ab68edfc9d743dcce4afca6715582d653fee
Dominic Scheurer <scheurer@cs.tu-darmstadt.de>
2016-07-07 10:46:49 +0200

    [Refactoring] Removed recoder specifics from CCSourceElement

--- key/key.common.core/src/org/key_project/common/core/program/CCSourceElement.java
+++ key/key.common.core/src/org/key_project/common/core/program/CCSourceElement.java
@@ -21,60 +21,6 @@ import org.key_project.common.core.logic.op.SVSubstitute;
  */
 public interface CCSourceElement<V extends ProgramVisitor, S extends CCSourceElement<V, S>>
         extends SVSubstitute {
-    /**
-     * Finds the source element that occurs first in the source.
-     * 
-     * @return the first source element in the syntactical representation of
-     *         this element, may be equals to this element.
-     * @see #getStartPosition()
-     */
-    S getFirstElement();
-
-    /**
-     * Finds the source element that occurs first in the source.
-     * 
-     * @return the first source element in the syntactical representation of
-     *         this element, may be equals to this element.
-     * @see #getStartPosition()
-     */
-    S getFirstElementIncludingBlocks();
-
-    /**
-     * Finds the source element that occurs last in the source.
-     * 
-     * @return the last source element in the syntactical representation of this
-     *         element, may be equals to this element.
-     * @see #getEndPosition()
-     */
-    S getLastElement();
-
-    /**
-     * Returns the start position of the primary token of this element. To get
-     * the start position of the syntactical first token, call the corresponding
-     * method of <CODE>getFirstElement()</CODE>.
-     * 
-     * @return the start position of the primary token.
-     */
-    Position getStartPosition();
-
-    /**
-     * Returns the end position of the primary token of this element. To get the
-     * end position of the syntactical first token, call the corresponding
-     * method of <CODE>getLastElement()</CODE>.
-     * 
-     * @return the end position of the primary token.
-     */
-    Position getEndPosition();
-
-    /**
-     * Returns the relative position (number of blank heading lines and columns)
-     * of the primary token of this element. To get the relative position of the
-     * syntactical first token, call the corresponding method of
-     * <CODE>getFirstElement()</CODE>.
-     * 
-     * @return the relative position of the primary token.
-     */
-    Position getRelativePosition();
 
     /**
      * calls the corresponding method of a visitor in order to perform some
--- key/key.core/src/de/uka/ilkd/key/java/SourceElement.java
+++ key/key.core/src/de/uka/ilkd/key/java/SourceElement.java
@@ -16,25 +16,84 @@ package de.uka.ilkd.key.java;
 import java.io.IOException;
 
 import org.key_project.common.core.program.CCSourceElement;
+import org.key_project.common.core.program.Position;
 
 import de.uka.ilkd.key.java.visitor.Visitor;
 
 /**
- *  A source element is a piece of syntax. It does not necessarily have
- *  a semantics, at least none that is machinable, for instance a
- *  {@link recoder.java.Comment}.
- * taken from RECODER and changed to achieve an immutable structure
+ * A source element is a piece of syntax. It does not necessarily have a
+ * semantics, at least none that is machinable, for instance a
+ * {@link recoder.java.Comment}. taken from RECODER and changed to achieve an
+ * immutable structure
  */
 
 public interface SourceElement extends CCSourceElement<Visitor, SourceElement> {
 
     /**
      * Pretty print.
-     * @param w a pretty printer.
-     * @exception IOException occasionally thrown.
+     * 
+     * @param w
+     *            a pretty printer.
+     * @exception IOException
+     *                occasionally thrown.
      */
     void prettyPrint(PrettyPrinter w) throws java.io.IOException;
 
     PositionInfo getPositionInfo();
+
+    /**
+     * Finds the source element that occurs first in the source.
+     * 
+     * @return the first source element in the syntactical representation of
+     *         this element, may be equals to this element.
+     * @see #getStartPosition()
+     */
+    SourceElement getFirstElement();
+
+    /**
+     * Finds the source element that occurs first in the source.
+     * 
+     * @return the first source element in the syntactical representation of
+     *         this element, may be equals to this element.
+     * @see #getStartPosition()
+     */
+    SourceElement getFirstElementIncludingBlocks();
+
+    /**
+     * Finds the source element that occurs last in the source.
+     * 
+     * @return the last source element in the syntactical representation of this
+     *         element, may be equals to this element.
+     * @see #getEndPosition()
+     */
+    SourceElement getLastElement();
+
+    /**
+     * Returns the start position of the primary token of this element. To get
+     * the start position of the syntactical first token, call the corresponding
+     * method of <CODE>getFirstElement()</CODE>.
+     * 
+     * @return the start position of the primary token.
+     */
+    Position getStartPosition();
+
+    /**
+     * Returns the end position of the primary token of this element. To get the
+     * end position of the syntactical first token, call the corresponding
+     * method of <CODE>getLastElement()</CODE>.
+     * 
+     * @return the end position of the primary token.
+     */
+    Position getEndPosition();
+
+    /**
+     * Returns the relative position (number of blank heading lines and columns)
+     * of the primary token of this element. To get the relative position of the
+     * syntactical first token, call the corresponding method of
+     * <CODE>getFirstElement()</CODE>.
+     * 
+     * @return the relative position of the primary token.
+     */
+    Position getRelativePosition();
 
 }
