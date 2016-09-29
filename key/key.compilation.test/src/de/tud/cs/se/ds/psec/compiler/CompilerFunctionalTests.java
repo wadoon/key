c5d3de3368ef3c8ac8242a2bd46ad753a34ae5c6
Dominic Scheurer <scheurer@cs.tu-darmstadt.de>
2016-09-29 17:01:20 +0200

    Created first functional test case

--- key/key.compilation.test/src/de/tud/cs/se/ds/psec/compiler/CompilerFunctionalTests.java
+++ key/key.compilation.test/src/de/tud/cs/se/ds/psec/compiler/CompilerFunctionalTests.java
@@ -1,5 +1,15 @@
 package de.tud.cs.se.ds.psec.compiler;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+import org.key_project.util.java.IOUtil;
+
+import de.tud.cs.se.ds.psec.parser.exceptions.TranslationTacletInputException;
+import de.uka.ilkd.key.proof.io.ProblemLoaderException;
 import junit.framework.TestCase;
 
 /**
@@ -9,17 +19,33 @@ import junit.framework.TestCase;
  * @author Dominic Scheurer
  */
 public class CompilerFunctionalTests extends TestCase {
+    private static final String FUNCTIONAL_TESTS_RELATIVE_DIR = "/resources/testcase/functional/";
+    private static final String TMP_OUT_DIR = "./testTmp/";
+
+    private String functionalTestsDir;
 
     @Override
     protected void setUp() throws Exception {
-        // TODO Auto-generated method stub
-        super.setUp();
+        Files.createDirectories(Paths.get(TMP_OUT_DIR));
+
+        File projectRoot = IOUtil.getProjectRoot(CompilerFunctionalTests.class);
+        functionalTestsDir = projectRoot + FUNCTIONAL_TESTS_RELATIVE_DIR;
     }
-    
+
     @Override
     protected void tearDown() throws Exception {
-        // TODO Auto-generated method stub
-        super.tearDown();
+        Files.delete(Paths.get(TMP_OUT_DIR));
     }
-    
+
+    @Test
+    public void testTest() throws TranslationTacletInputException,
+            ProblemLoaderException, IOException {
+
+        Compiler compiler = new Compiler(
+                new File(functionalTestsDir, "SimpleArithmeticAndIf.java"),
+                TMP_OUT_DIR, true, true, false);
+
+        compiler.compile();
+    }
+
 }
