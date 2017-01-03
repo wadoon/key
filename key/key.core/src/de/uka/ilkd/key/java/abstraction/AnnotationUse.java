f72a1c69df627f3473305d1ec1c6fc30dde79d35
greiner <Simon.Greiner@kit.edu>
2017-01-03 12:16:50 +0100

    missing files

---
+++ key/key.core/src/de/uka/ilkd/key/java/abstraction/AnnotationUse.java
@@ -0,0 +1,10 @@
+package de.uka.ilkd.key.java.abstraction;
+
+import recoder.abstraction.ElementValuePair;
+
+public interface AnnotationUse {
+
+    
+    java.util.List<? extends ElementValuePair> getElementValuePairs();
+    
+}
