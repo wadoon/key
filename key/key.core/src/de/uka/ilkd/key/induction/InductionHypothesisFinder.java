ead74653280cda8725c21d35b6adf64975f1dcb2
Roettger <#Rdafxnav5>
2017-03-10 12:32:03 +0100

    Added the class InductionHypothesisFinder to the package de.uka.ilkd.key.induction to find the induction hypothesis of a given all quantified term.

---
+++ key/key.core/src/de/uka/ilkd/key/induction/InductionHypothesisFinder.java
@@ -0,0 +1,17 @@
+package de.uka.ilkd.key.induction;
+
+import org.key_project.util.collection.ImmutableSet;
+
+import de.uka.ilkd.key.logic.sort.Sort;
+
+public class InductionHypothesisFinder {
+
+	public InductionHypothesisFinder() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ImmutableSet<Sort> collectTypesFromTerm(Term allQuantifiedTerm){
+		return null;
+	}
+
+}
