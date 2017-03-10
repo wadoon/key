d85c859ea330eb578006b543eeee5310edf58c75
Roettger <#Rdafxnav5>
2017-03-10 12:45:17 +0100

    Added the method collectTypesFromTerm to the class InductionHypothesisFinder to get a ImmutableSet<Sort> of all the variable types used in the given term.

--- key/key.core/src/de/uka/ilkd/key/induction/InductionHypothesisFinder.java
+++ key/key.core/src/de/uka/ilkd/key/induction/InductionHypothesisFinder.java
@@ -1,7 +1,10 @@
 package de.uka.ilkd.key.induction;
 
+import org.key_project.util.collection.ImmutableArray;
 import org.key_project.util.collection.ImmutableSet;
 
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.op.QuantifiableVariable;
 import de.uka.ilkd.key.logic.sort.Sort;
 
 public class InductionHypothesisFinder {
@@ -10,7 +13,21 @@ public class InductionHypothesisFinder {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * 
+	 * @param allQuantifiedTerm is a term quantified by the "forall" quantifier. Note that 
+	 * the "forall" quantifier is not needed for this function to work correctly. This function is 
+	 * just mostly used in this context.
+ 	 * @return An ImmutableSet&lt;Sort&gt; the types of all variables in the given formula
+	 */
 	public ImmutableSet<Sort> collectTypesFromTerm(Term allQuantifiedTerm){
+		ImmutableSet<QuantifiableVariable> variables = new ImmutableSet<QuantifiableVariable>();
+		
+		//get all variables from the given term
+		ImmutableArray<QuantifiableVariable> boundVars = allQuantifiedTerm.boundVars();
+		ImmutableSet<QuantifiableVariable> freeVars = allQuantifiedTerm.freeVars();
+		
+		
 		return null;
 	}
 
