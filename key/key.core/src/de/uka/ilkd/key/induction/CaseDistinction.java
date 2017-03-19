27c70b054458508eeec908e87f0a8c81cc01a16d
tRoettger <tobias.roettger@gmx.net>
2017-03-19 12:26:13 +0100

    Added the class ConstructorExtractor to extract the constructors of all sorts in a given term.

--- key/key.core/src/de/uka/ilkd/key/induction/CaseDistinction.java
+++ key/key.core/src/de/uka/ilkd/key/induction/CaseDistinction.java
@@ -1,9 +1,16 @@
 package de.uka.ilkd.key.induction;
 
+import java.util.LinkedList;
+
+import de.uka.ilkd.key.logic.op.QuantifiableVariable;
+
 public class CaseDistinction {
 
-	public CaseDistinction() {
-		// TODO Auto-generated constructor stub
+	private QuantifiableVariable variable;
+	private LinkedList<Case> cases;
+	
+	public CaseDistinction(QuantifiableVariable v, LinkedList<Case> c) {
+		this.variable = v;
 	}
 
 }
---
+++ key/key.core/src/de/uka/ilkd/key/induction/ConstructorExtractor.java
@@ -0,0 +1,9 @@
+package de.uka.ilkd.key.induction;
+
+public class ConstructorExtractor {
+
+	public ConstructorExtractor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
