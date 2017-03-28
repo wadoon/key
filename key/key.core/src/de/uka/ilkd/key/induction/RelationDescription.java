b027994bfa850829a1776502874da8a72cdbbc19
tRoettger <tobias.roettger@gmx.net>
2017-03-28 03:50:53 +0200

    Created the class RelationDescription for the creation of AtomicRelationDescriptions.

--- key/key.core/src/de/uka/ilkd/key/induction/RelationDescription.java
+++ key/key.core/src/de/uka/ilkd/key/induction/RelationDescription.java
@@ -4,12 +4,17 @@ import java.util.LinkedList;
 
 import org.key_project.util.collection.DefaultImmutableMap;
 import org.key_project.util.collection.ImmutableArray;
+import org.key_project.util.collection.ImmutableList;
 import org.key_project.util.collection.ImmutableMap;
 
 import de.uka.ilkd.key.java.Services;
+import de.uka.ilkd.key.logic.Named;
+import de.uka.ilkd.key.logic.Namespace;
 import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.op.Function;
 import de.uka.ilkd.key.logic.op.QuantifiableVariable;
+import de.uka.ilkd.key.rule.FindTaclet;
+import de.uka.ilkd.key.rule.Rule;
 import de.uka.ilkd.key.strategy.quantifierHeuristics.Substitution;
 
 public class RelationDescription {
@@ -20,10 +25,31 @@ public class RelationDescription {
 	public RelationDescription(Term t, Services serv) {
 		ConstructorExtractor ce = new ConstructorExtractor(t, serv);
 		ImmutableArray<Function> constructors = ce.getConstructors();
+		LinkedList<Term> rangeFormulas;
 		for(Function f : constructors){
 			possibleSubstitutions.add(createSubstitution(f, serv));
+	}
+		rangeFormulas = createRangeFormulas(t, serv);
+	}
+	
+	private static LinkedList<Term> createRangeFormulas(Term t, Services s){
+		ImmutableList<Named> namedrules = s.getNamespaces().ruleSets().elements();
+		LinkedList<FindTaclet> rules = new LinkedList<FindTaclet>();
+		for(Named n : namedrules){
+			Rule r = (Rule)n;
+			if(r instanceof FindTaclet){
+				FindTaclet ft = (FindTaclet)r;
+				if(findTacletMatches(ft, t)){
+					rules.add(ft);
+				}
+			}
 		}
-		
+		return null;
+	}
+	
+	private static boolean findTacletMatches(FindTaclet ft, Term t){
+		//TODO: return whether the given term fits to the findtaclet or not.
+		return false;
 	}
 	
 	private static Substitution createSubstitution(Function f, Services s){
