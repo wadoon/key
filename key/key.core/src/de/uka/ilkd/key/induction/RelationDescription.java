ae2d42848513eaef9320d949f20fa15ff710cb25
tRoettger <tobias.roettger@gmx.net>
2017-04-04 01:46:02 +0200

    Changed the method createSubstitution to createSubstitutions.
    The method now gives every possible substitution for the given function.

--- key/key.core/src/de/uka/ilkd/key/induction/RelationDescription.java
+++ key/key.core/src/de/uka/ilkd/key/induction/RelationDescription.java
@@ -10,26 +10,30 @@ import org.key_project.util.collection.ImmutableMap;
 import de.uka.ilkd.key.java.Services;
 import de.uka.ilkd.key.logic.Named;
 import de.uka.ilkd.key.logic.Namespace;
+import de.uka.ilkd.key.logic.NamespaceSet;
 import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.TermBuilder;
 import de.uka.ilkd.key.logic.op.Function;
 import de.uka.ilkd.key.logic.op.QuantifiableVariable;
+import de.uka.ilkd.key.logic.sort.Sort;
 import de.uka.ilkd.key.rule.FindTaclet;
 import de.uka.ilkd.key.rule.MatchConditions;
 import de.uka.ilkd.key.rule.Rule;
 import de.uka.ilkd.key.strategy.quantifierHeuristics.Substitution;
+import de.uka.ilkd.key.util.Pair;
 
 public class RelationDescription {
 
 	private LinkedList<AtomicRelationDescription> atomics;
-	private LinkedList<Substitution> possibleSubstitutions;
+	private LinkedList<Pair<QuantifiableVariable, Term>> possibleSubstitutions;
 	
-	public RelationDescription(Term t, Services serv) {
+	public RelationDescription(Term t, Services serv){
 		ConstructorExtractor ce = new ConstructorExtractor(t, serv);
 		ImmutableArray<Function> constructors = ce.getConstructors();
 		LinkedList<Term> rangeFormulas;
 		for(Function f : constructors){
-			possibleSubstitutions.add(createSubstitution(f, serv));
-	}
+			possibleSubstitutions.add(createSubstitutions(f, serv));
+		}
 		rangeFormulas = createRangeFormulas(t, serv);
 	}
 	
@@ -46,6 +50,7 @@ public class RelationDescription {
 				}
 			}
 		}
+		//TODO: compute the range formula
 		return null;
 	}
 	
@@ -56,11 +61,25 @@ public class RelationDescription {
 		return mc != null;
 	}
 	
-	private static Substitution createSubstitution(Function f, Services s){
+	private static LinkedList<Pair<QuantifiableVariable, Term>> createSubstitutions(Function f, Services s){
 		//How to create a Substitution and new QuantifiableVariables.
 		//TODO: return a substitution with a new QuantifiableVariable and the function with new variables as term.
-		return null;
+		Sort returnSort = f.sort();
+		Namespace vars = s.getNamespaces().variables();
+		TermBuilder tb = s.getTermBuilder();
+		Term t = tb.func(f);
+		LinkedList<Pair<QuantifiableVariable, Term>> substitutions = new LinkedList<Pair<QuantifiableVariable, Term>>();
+		QuantifiableVariable qv = null;
+		for(Named n : vars.elements()){
+			if(n instanceof QuantifiableVariable){
+				qv = (QuantifiableVariable)n;
+				if(qv.sort().equals(returnSort)){
+					substitutions.add(new Pair<QuantifiableVariable, Term>(qv, t));
+				}
+			}
+		}
+		
+		return substitutions;
 	}
 	
-
 }
