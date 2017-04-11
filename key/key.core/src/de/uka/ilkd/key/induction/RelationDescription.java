95433ca2b212a0edaf95ccb996ed43534fbfed07
tRoettger <tobias.roettger@gmx.net>
2017-04-11 15:34:47 +0200

    Created TacletGenTest class to practice the generation of taclets.
    Worked on the implementation of RelationDescription to improve the
    computation of the substitutions (not finished yet).

--- key/key.core/src/de/uka/ilkd/key/induction/RelationDescription.java
+++ key/key.core/src/de/uka/ilkd/key/induction/RelationDescription.java
@@ -35,16 +35,21 @@ public class RelationDescription {
 		ConstructorExtractor ce = new ConstructorExtractor(t, serv);
 		ImmutableArray<Function> constructors = ce.getConstructors();
 		for(Function f : constructors){
-			possibleSubstitutions.addAll(createSubstitutions(f, serv));
+			try{
+				possibleSubstitutions.addAll(createSubstitutions(f, serv));
+			} catch (NullPointerException npe){
+				//no substitutions for the function f were found
+				//TODO: [optional] show the user that there are no rules for this function (maybe ask whether intended or not). 
+			}
 		}
 		possibleRangeFormulas = createRangeFormulas(t, serv);
 		
-		//TODO: generate AtomicRelationDescription for all possibleRangeFormulas
 		for(Term range : possibleRangeFormulas){
 			/*
 			 * How to select the right substitutions? Is another ConstructorExctractor needed?
 			 * If yes is the one above needed?
 			 */
+			System.out.println("\tAdded range: " + range.toString());
 			atomics.add(new AtomicRelationDescription(
 					range,
 					possibleSubstitutions	//TODO: filter this list.
@@ -62,28 +67,30 @@ public class RelationDescription {
 		TermBuilder tb = s.getTermBuilder();
 		//TODO:[optional] check for optimizations
 		for(Named n : namedrules){
-			Rule r = (Rule)n;
-			if(r instanceof FindTaclet){
-				FindTaclet ft = (FindTaclet)r;
-				//check whether the find term of the the FindTaclet is an instance of the given term
-				Term rangeFormula = createRangeFormula(t, ft.find(), s);
-				//TODO:[optional] find a way to express multiple rangeformulas in one (optimization)
-				/*
-				 * E.g. if there are rangeformulas int x: x = 0, x = 1, x = 2
-				 * make a new rangeformula x >= 0 && x <= 2 and throw the others away.
-				 */
-				
-				//TODO:[optional] check for optimization
-				int nos = rangeFormula.subs().size();
-				boolean falseIsDirectSubterm = false;
-				for(int i = 0; i < nos; i++){
-					if(rangeFormula.sub(i).equals(tb.ff())){
-						falseIsDirectSubterm = true;	//does the "and" operator work with this?
-						break;							//@see createRangeFormula
+			if(n instanceof Rule){
+				Rule r = (Rule)n;
+				if(r instanceof FindTaclet){
+					FindTaclet ft = (FindTaclet)r;
+					//check whether the find term of the the FindTaclet is an instance of the given term
+					Term rangeFormula = createRangeFormula(t, ft.find(), s);
+					//TODO:[optional] find a way to express multiple rangeformulas in one (optimization)
+					/*
+					 * E.g. if there are rangeformulas int x: x = 0, x = 1, x = 2
+					 * make a new rangeformula x >= 0 && x <= 2 and throw the others away.
+					 */
+					
+					//TODO:[optional] check for optimization
+					int nos = rangeFormula.subs().size();
+					boolean falseIsDirectSubterm = false;
+					for(int i = 0; i < nos; i++){
+						if(rangeFormula.sub(i).equals(tb.ff())){
+							falseIsDirectSubterm = true;	//does the "and" operator work with this?
+							break;							//@see createRangeFormula
+						}
 					}
-				}
-				if(!falseIsDirectSubterm){
-					possibleRangeFormulas.add(rangeFormula);
+					if(!falseIsDirectSubterm){
+						possibleRangeFormulas.add(rangeFormula);
+					}
 				}
 			}
 		}
@@ -126,22 +133,110 @@ public class RelationDescription {
 	 * @return a LinkedList&lt;Pair&lt;QuantifiableVariable, Term&gt;&gt; which contains constructor substitutions
 	 */
 	private static LinkedList<Pair<QuantifiableVariable, Term>> createSubstitutions(Function f, Services s){
+		
+		//ONLY FOR TESTING
+		System.out.println("Function: " + f.toString());
+		
 		Sort returnSort = f.sort();
 		Namespace vars = s.getNamespaces().variables();
 		TermBuilder tb = s.getTermBuilder();
-		Term t = tb.func(f);
+		LinkedList<QuantifiableVariable[]> parameters = new LinkedList<QuantifiableVariable[]>();
+		Term t;
 		LinkedList<Pair<QuantifiableVariable, Term>> substitutions = new LinkedList<Pair<QuantifiableVariable, Term>>();
 		QuantifiableVariable qv = null;
+		LinkedList<QuantifiableVariable> qvs = new LinkedList<QuantifiableVariable>();
+		
+		if(f.arity() == 0){
+			return null;
+		}
+		
+		LinkedList<QuantifiableVariable>[] possibleParameters = new LinkedList[f.arity()]; 
+		
+		//transform Named to QuantifiableVariable
 		for(Named n : vars.elements()){
 			if(n instanceof QuantifiableVariable){
-				qv = (QuantifiableVariable)n;
-				if(qv.sort().equals(returnSort)){
-					substitutions.add(new Pair<QuantifiableVariable, Term>(qv, t));
+				qvs.add((QuantifiableVariable)n);
+			}
+		}
+		
+		//TODO: [optional] check for optimization. The for loop should more or less do the same.
+		for(int i = 0; i < f.arity(); i++){
+			Sort sortAtPosition = f.argSort(i);
+			possibleParameters[i] = getAllVariablesOfSort(qvs, sortAtPosition);
+		}
+
+		parameters = allCombinations(possibleParameters, 0);
+		
+		for(QuantifiableVariable[] combination : parameters){
+			t  = tb.func(f, varsToTerm(combination, tb), new ImmutableArray<QuantifiableVariable>());
+			for(QuantifiableVariable q : qvs){
+				if(q.sort().equals(returnSort)){
+					substitutions.add(new Pair<QuantifiableVariable, Term>(q, t));
 				}
 			}
 		}
 		
+		
+		
 		return substitutions;
 	}
 	
+	/**
+	 * 
+	 * @param qvs a LinkedList&lt;QuantifiableVariables&gt;
+	 * @param s a Sort
+	 * @return a LinkedList&lt;QuantifiableVariable&gt; which only contains QuantifiableVariables of the given Sort.
+	 */
+	private static LinkedList<QuantifiableVariable> getAllVariablesOfSort(LinkedList<QuantifiableVariable> qvs, Sort s){
+		//TODO: [optional] check for optimization
+		LinkedList<QuantifiableVariable> filtered = new LinkedList<QuantifiableVariable>();
+		for(QuantifiableVariable qv : qvs){
+			if(qv.sort().equals(s)){
+				filtered.add(qv);
+			}
+		}
+		return filtered;
+	}
+	
+	/**
+	 * 
+	 * @param args
+	 * @param offset
+	 * @return a list of combinations
+	 */
+	private static <T> LinkedList<T[]> allCombinations(LinkedList<T>[] args, int offset){
+		LinkedList<T[]> combis = new LinkedList<T[]>();
+			
+		if(offset < args.length){
+			for(T t : args[offset]){
+				for(T[] subcombi : allCombinations(args, offset + 1)){
+					LinkedList<T> arrayAsList = new LinkedList<T>();
+					arrayAsList.add(t);
+					for(int i = 0; i < subcombi.length; i++){
+						arrayAsList.add(subcombi[i]);
+					}
+					combis.add((T[]) arrayAsList.toArray());
+				}
+			}
+		}
+		else{
+			combis.add((T[])args[offset].toArray());
+		}
+		
+		return combis;
+	}
+	
+	/**
+	 * 
+	 * @param qvs
+	 * @param tb
+	 * @return the terms which only consist of the given variables.
+	 */
+	private static Term[] varsToTerm(QuantifiableVariable[] qvs, TermBuilder tb){
+		Term[] terms = new Term[qvs.length];
+		for(int i = 0; i < qvs.length; i++){
+			terms[i] = tb.var(qvs[i]);
+		}
+		return terms;
+	}
 }
---
+++ key/key.core/src/de/uka/ilkd/key/induction/TacletGenTest.java
@@ -0,0 +1,21 @@
+package de.uka.ilkd.key.induction;
+
+import de.uka.ilkd.key.java.Services;
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.rule.tacletbuilder.TacletGenerator;
+
+public class TacletGenTest {
+
+	private Term term;
+	private Services services;
+	
+	public TacletGenTest(Term t, Services s) {
+		term = t;
+		services = s;
+	}
+	
+	public void tacletGen(){
+		TacletGenerator generator;
+	}
+
+}
