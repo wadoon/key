8f17e57f3ba012bb62cde4cf21bb13dd32926eb6
tRoettger <tobias.roettger@gmx.net>
2017-04-18 02:42:01 +0200

    Implemented the method tacletGen() for class TacletGenTest

--- key/key.core/src/de/uka/ilkd/key/induction/TacletGenTest.java
+++ key/key.core/src/de/uka/ilkd/key/induction/TacletGenTest.java
@@ -1,7 +1,13 @@
 package de.uka.ilkd.key.induction;
 
+import org.key_project.util.collection.ImmutableList;
+
 import de.uka.ilkd.key.java.Services;
+import de.uka.ilkd.key.logic.Name;
 import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.op.ProgramVariable;
+import de.uka.ilkd.key.rule.RuleSet;
+import de.uka.ilkd.key.rule.Taclet;
 import de.uka.ilkd.key.rule.tacletbuilder.TacletGenerator;
 
 public class TacletGenTest {
@@ -15,7 +21,24 @@ public class TacletGenTest {
 	}
 	
 	public void tacletGen(){
-		TacletGenerator generator;
+		TacletGenerator generator = getTacletGenerator();
+		
+		Name tacletName = new Name("testtaclet");
+		
+		ImmutableList<ProgramVariable> programVars = null;
+		Taclet tac = generator.generateRewriteTaclet(
+				tacletName, 
+				term,
+				term, 
+				programVars, 
+				services.getNamespaces().ruleSets(), 
+				services
+				);
+	}
+	
+	private TacletGenerator getTacletGenerator(){
+		//TODO: get a tacletgenerator.
+		return TacletGenerator.getInstance();
 	}
 
 }
