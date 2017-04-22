de495ea66c0fa86c93254e042b5cfd4e61ad0f15
Alexander Weigl <weigl@kit.edu>
2017-04-22 11:59:09 +0200

    small changes on the api

--- key/key.core/src/de/uka/ilkd/key/api/ProjectedNode.java
+++ key/key.core/src/de/uka/ilkd/key/api/ProjectedNode.java
@@ -56,4 +56,8 @@ public class ProjectedNode {
     public boolean isPseudoNode() {
         return proofNode == null;
     }
+
+    Node getProofNode() {
+        return proofNode;
+    }
 }
--- key/key.core/src/de/uka/ilkd/key/api/VariableAssignments.java
+++ key/key.core/src/de/uka/ilkd/key/api/VariableAssignments.java
@@ -1,8 +1,34 @@
 package de.uka.ilkd.key.api;
 
+import de.uka.ilkd.key.macros.scripts.meta.Type;
+
 /**
  * Created by sarah on 4/21/17.
  */
 public class VariableAssignments {
     //hashmaps mit variablen zuweisungen und pointer zu eltern hashmap
+    private VariableAssignments parent;
+
+    public VariableAssignments() {
+    }
+
+    public VariableAssignments(VariableAssignments parent) {
+        this.parent = parent;
+    }
+
+    public Object getValue(String name) {
+        return null;
+    }
+
+    public Type getType() {
+        return null;
+    }
+
+    public void setType(Type type) {
+
+    }
+
+    public void setValue(String name) {
+
+    }
 }
