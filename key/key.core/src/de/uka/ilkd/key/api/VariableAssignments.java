c58c1eb5548c63a78a16be551f790b7f072445fa
Sarah Grebing <grebing@ira.uka.de>
2017-04-27 08:50:13 +0200

    Variable Assignment Class

--- key/key.core/src/de/uka/ilkd/key/api/VariableAssignments.java
+++ key/key.core/src/de/uka/ilkd/key/api/VariableAssignments.java
@@ -1,22 +1,117 @@
 package de.uka.ilkd.key.api;
-
-import de.uka.ilkd.key.macros.scripts.meta.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by sarah on 4/21/17.
+ * Class to represent current Variable assigments.
+ * Created by S.Grebing
  */
 public class VariableAssignments {
-    //hashmaps mit variablen zuweisungen und pointer zu eltern hashmap
+
+
+    public enum VarType {
+        INT,
+        BOOL,
+        ANY,
+        TERM,
+        OBJECT;
+    }
+
+    /**
+     * Reference to parent assignments
+     */
     private VariableAssignments parent;
 
+    /**
+     * Current Assignments
+     */
+    private Map<String, Object> currentAssignments;
+
+    /**
+     * Type Map of assignments
+     */
+    private Map<String, VariableAssignments.VarType> typeMap;
+    /**
+     * Create new, empty variable assignment, to add variables
+     * @param parent
+     */
+    public VariableAssignments(VariableAssignments parentAssignments){
+        this.currentAssignments = new HashMap<>();
+        this.typeMap = new HashMap<>();
+        this.parent = parentAssignments;
+    }
+
+
+    /**
+     * Default constructor
+     */
     public VariableAssignments() {
+        this.currentAssignments = new HashMap<>();
+        this.typeMap = new HashMap<>();
+        this.parent = null;
     }
 
-    public VariableAssignments(VariableAssignments parent) {
-        this.parent = parent;
+
+    /**
+     * Get the value of a stored variable name
+     * TODO Exception spezifischer
+     * @param varName
+     * @return Value of variable
+     */
+    public Object getVarValue(String varName) throws Exception {
+        if(currentAssignments.containsKey(varName)){
+            return currentAssignments.get(varName);
+        }else{
+            if(parent !=null) {
+                return parent.getVarValue(varName);
+            }else{
+                throw new Exception("Variable "+varName+" could not be found");
+            }
+        }
     }
 
-    public Object getValue(String name) {
+    /**
+     * Add a variable assignment with type and value
+     * @param varName
+     * @param value
+     * @param type
+     */
+    public void addAssignmentWithType(String varName, Object value, VarType type){
+        typeMap.put(varName, type);
+        currentAssignments.put(varName, value);
+
+    }
+
+    /**
+     * Add a variable assignment without type
+     * @param varName
+     * @param value
+     */
+    public void addAssignment(String varName, Object value) throws Exception {
+        if(typeMap.containsKey(varName)) {
+            currentAssignments.put(varName, value);
+        } else{
+            throw new Exception("Variable "+ varName +"must be declared first");
+        }
+
+    }
+
+    /**
+     * TODO better exception
+     * Add a new type declaration
+     * @param varName
+     * @param type
+     */
+    public void addType(String varName, VarType type) throws Exception {
+        if(typeMap.containsKey(varName)){
+            if(typeMap.get(varName) != type){
+                throw new Exception("Variable "+varName+ "was already declared with type " + typeMap.get(varName).toString());
+            }
+        }else{
+            typeMap.put(varName, type);
+        }
+    }
+/*    public Object getValue(String name) {
         return null;
     }
 
@@ -30,5 +125,5 @@ public class VariableAssignments {
 
     public void setValue(String name) {
 
-    }
+    }*/
 }
