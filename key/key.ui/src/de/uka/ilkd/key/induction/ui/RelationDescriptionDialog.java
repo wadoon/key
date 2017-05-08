09bd726d31bfcb75908411e34baddb4402d904c6
Roettger <#Rdafxnav5>
2017-05-08 11:02:05 +0200

    Put the content of this window in a JScrollPane.
    Adjusted the size and the position of the dialog (Size: 800x600 Pixel,
    Position in the middle of the screen).

--- key/key.ui/src/de/uka/ilkd/key/induction/ui/RelationDescriptionDialog.java
+++ key/key.ui/src/de/uka/ilkd/key/induction/ui/RelationDescriptionDialog.java
@@ -3,12 +3,15 @@ package de.uka.ilkd.key.induction.ui;
 import java.awt.Dialog;
 import java.awt.Frame;
 import java.awt.GraphicsConfiguration;
+import java.awt.Toolkit;
+import java.awt.Dimension;
 import java.awt.Window;
 import java.util.LinkedList;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import org.key_project.util.collection.ImmutableList;
@@ -23,6 +26,9 @@ import de.uka.ilkd.key.rule.TacletApp;
 
 public class RelationDescriptionDialog extends JDialog {
 
+	private static final int DIALOG_WIDTH = 800;
+	private static final int DIALOG_HEIGHT = 600;
+	
 	private JLabel headline;
 	private JLabel content;
 	private RelationDescription relationdescription;
@@ -30,8 +36,14 @@ public class RelationDescriptionDialog extends JDialog {
 	public RelationDescriptionDialog(JFrame parent, Term term, Services s) {
 		super(parent, "Atomic Relation Descriptions");
 		
+		Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+		
 		this.setVisible(true);
-		this.setSize(350, 175);
+		this.setSize(DIALOG_WIDTH, DIALOG_HEIGHT);
+		this.setLocation(
+				(screenDim.width - DIALOG_WIDTH) / 2, 
+				(screenDim.height - DIALOG_HEIGHT) / 2
+		);
 		
 		this.headline = new JLabel("Relationen Descriptions for " + term.toString());
 		this.content = new JLabel();
@@ -45,20 +57,20 @@ public class RelationDescriptionDialog extends JDialog {
 
 	private void displayRelationDescriptions(){
 		LinkedList<AtomicRelationDescription> loard = relationdescription.getAtomics();
-		
+		JTextArea textarea = new JTextArea(30,10);
+		JScrollPane scrollpane;
 		StringBuilder sb = new StringBuilder();
 		for(AtomicRelationDescription ard : loard){
 			sb.append(ard.toString());
 			sb.append("\n\n");
 		}
 		
-		JTextArea textarea = new JTextArea(30,10);
 		textarea.setLineWrap(true);
 		textarea.setWrapStyleWord(true);
 		textarea.setText(sb.toString());
 		textarea.setEditable(false);
-		
-		this.add(textarea);
+		scrollpane  = new JScrollPane(textarea);
+		this.add(scrollpane);
 		
 	}
 }
