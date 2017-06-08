d966506d4b18c99e7f04e8bb5c4cf553a9d39d78
Dominic Scheurer <scheurer@cs.tu-darmstadt.de>
2017-06-08 17:04:58 +0200

    [Bug Fix] Branch labels get lost when pruning -- not any longer

--- key/key.core/src/de/uka/ilkd/key/proof/Proof.java
+++ key/key.core/src/de/uka/ilkd/key/proof/Proof.java
@@ -777,6 +777,8 @@ public class Proof implements Named {
         if(getGoal(cuttingPoint) != null || cuttingPoint.isClosed()){
             return null;
         }
+        
+        final String origBranchLabel = cuttingPoint.getNodeInfo().getBranchLabel();
 
         ProofPruner pruner = new ProofPruner();
         if (fireChanges) {
@@ -787,6 +789,11 @@ public class Proof implements Named {
             fireProofGoalsChanged();
             fireProofPruned(cuttingPoint);
         }
+        
+        // We have to reset the branch label, otherwise it will get lost when
+        // the NodeInfo is "refreshed" after pruning
+        cuttingPoint.getNodeInfo().setBranchLabel(origBranchLabel);
+        
         return result;
     }
 
