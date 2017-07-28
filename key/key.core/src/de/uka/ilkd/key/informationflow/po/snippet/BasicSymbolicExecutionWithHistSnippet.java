2c1ec3d79c9fd93c7a30170b7caa1fda56eeead9
Jonas Krämer <urdea@student.kit.edu>
2017-07-28 15:56:08 +0200

    Remove some clutter from dc po.

--- key/key.core/src/de/uka/ilkd/key/informationflow/po/snippet/BasicSymbolicExecutionWithHistSnippet.java
+++ key/key.core/src/de/uka/ilkd/key/informationflow/po/snippet/BasicSymbolicExecutionWithHistSnippet.java
@@ -31,7 +31,7 @@ public class BasicSymbolicExecutionWithHistSnippet
                 poVars.pre.exception));
         posts = posts.append(d.tb.equals(poVars.post.heap, d.tb.getBaseHeap()));
         posts = posts.append(helperFactory.defineTermVar());
-        posts = posts.append(helperFactory.postHistoryEquality());
+        //posts = posts.append(helperFactory.postHistoryEquality());
         posts = posts.append(helperFactory.postInternalHistoryEquality());
         
         final Term prog = buildProgramTerm(d, poVars, d.tb.and(posts), d.tb);
