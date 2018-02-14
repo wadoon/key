f6299b91fc9bd87d6f2d411e3ae4c93de6c6a86a
Sarah Grebing <grebing@ira.uka.de>
2018-02-14 09:06:26 +0100

    Bugfix retention policy for Varargs Annotations

--- key/key.core/src/de/uka/ilkd/key/macros/scripts/meta/Varargs.java
+++ key/key.core/src/de/uka/ilkd/key/macros/scripts/meta/Varargs.java
@@ -1,9 +1,13 @@
 package de.uka.ilkd.key.macros.scripts.meta;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * @author Alexander Weigl
  * @version 1 (02.05.17)
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Varargs {
     Class as() default String.class;
     String prefix() default "";
