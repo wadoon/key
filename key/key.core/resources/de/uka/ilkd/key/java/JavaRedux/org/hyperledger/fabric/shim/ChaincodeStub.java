29a11b09a35f3f65df31aff3f00a1bc334513ae3
Mihai Herda <herda@kit.edu>
2018-07-13 19:03:08 +0200

    Added sort and function declarations for the RPS-Game example.

--- key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/ChaincodeStub.java
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/ChaincodeStub.java
@@ -7,8 +7,6 @@ SPDX-License-Identifier: Apache-2.0
 package org.hyperledger.fabric.shim;
 
 public interface ChaincodeStub {
-
-
         //@ public instance ghost \seq transactionLog;
 
         //@ public instance model \map globalState;
@@ -16,13 +14,13 @@ public interface ChaincodeStub {
         //@ represents globalState = \dl_constructMap(transactionLog);
 
         /*@ public normal_behaviour
-            ensures \result == \dl_mapGet(globalState, key);
+            ensures \dl_array2seq(\result) == \dl_mapGet(globalState, key);
             assignable \strictly_nothing;
           @*/
 	byte[] getState(int key);
 
         /*@ public normal_behaviour
-            ensures transactionLog == \dl_seqConcat(\old(transactionLog), \dl_seqSingleton(\dl_newEntry(key,value)));
+            ensures transactionLog == \dl_seqConcat(\old(transactionLog), \dl_seqSingleton(\dl_newEntry(key,\dl_array2seq(value))));
             assignable transactionLog;
           @*/
 	void putState(int key, byte[] value);
