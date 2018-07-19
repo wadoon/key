3f295836ef5fdd7a4e4e5eb62fcad0a2bc5ef524
Mihai Herda <herda@kit.edu>
2018-07-19 11:21:57 +0200

    No longer using map for global state, added lastEntry(Seq,int) function instead.

--- key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/ChaincodeStub.java
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/ChaincodeStub.java
@@ -7,29 +7,26 @@ SPDX-License-Identifier: Apache-2.0
 package org.hyperledger.fabric.shim;
 
 public interface ChaincodeStub {
+
         //@ public instance ghost \seq transactionLog;
 
-        //@ public instance model \map globalState;
-
-        //@ represents globalState = \dl_constructMap(transactionLog);
-
         /*@ public normal_behaviour
-            ensures \dl_array2seq(\result) == \dl_mapGet(globalState, key);
-            assignable \strictly_nothing;
+            ensures \dl_array2seq(\result) == \dl_getValue(\dl_lastEntry(transactionLog, key));
+            assignable \nothing;
           @*/
-	byte[] getState(int key);
+	byte[] /*@helper@*/ getState(int key);
 
         /*@ public normal_behaviour
             ensures transactionLog == \dl_seqConcat(\old(transactionLog), \dl_seqSingleton(\dl_newEntry(key,\dl_array2seq(value))));
             assignable transactionLog;
           @*/
-	void putState(int key, byte[] value);
+	void /*@helper@*/ putState(int key, byte[] value);
 
         /*@ public normal_behaviour
             ensures transactionLog == \dl_seqConcat(\old(transactionLog), \dl_seqSingleton(\dl_newEntry(key,\dl_deleted)));
             assignable transactionLog;
           @*/
-	void delState(int key);
+	void /*@helper@*/ delState(int key);
 
 
 }
