3bacdc4f7c2051e9ef131a1ae52f54a1d056305e
Richard Bubel <bubel@cs.tu-darmstadt.de>
2018-08-19 11:53:32 +0200

    [Checkstyle] Hopefully shuts up checkstyle to complain about ObjectVal

--- key/key.core/src/de/uka/ilkd/key/smt/model/ObjectVal.java
+++ key/key.core/src/de/uka/ilkd/key/smt/model/ObjectVal.java
@@ -25,7 +25,7 @@ import de.uka.ilkd.key.logic.sort.Sort;
 
 /**
  * Represents an object inside a heap.
- * 
+ *
  * @author mihai
  *
  */
@@ -54,12 +54,17 @@ public class ObjectVal {
      * Maps array fields to array values.
      */
     private Map<Integer, String> arrayValues;
+
     /**
      * Maps function names to function values. Supported functions are model
      * fields and class invariant.
      */
     private Map<String, String> funValues;
 
+    /**
+     * creates a new object value of the given name
+     * @param name the Object's name
+     */
     public ObjectVal(String name) {
         this.name = name;
         fieldvalues = new HashMap<String, String>();
@@ -67,19 +72,35 @@ public class ObjectVal {
         funValues = new HashMap<String, String>();
     }
 
+    /**
+     * associate the function name with the given value
+     * @param fun the name of the function
+     * @param val the value to be associated with the specified function
+     */
     public void putFunValue(String fun, String val) {
         funValues.put(fun, val);
     }
 
+    /**
+     * set the i-th array element to the given value
+     * @param i the array index
+     * @param val the value
+     */
     public void setArrayValue(int i, String val) {
         arrayValues.put(i, val);
     }
 
+    /**
+     * returns the i-th array element
+     * @param i the index of the array element to be returned
+     * @return the value stores at index i
+     */
     public String getArrayValue(int i) {
         return arrayValues.get(i);
     }
 
     /**
+     * sets the (exact) dynamic type of this object
      * @param exactInstance
      *            the exactInstance to set
      */
@@ -87,40 +108,77 @@ public class ObjectVal {
         this.exactInstance = exactInstance;
     }
 
+    /**
+     * queries whether the (exact) dynamic type of this object is known
+     * @return
+     */
     public boolean isExactInstance() {
         return exactInstance;
     }
 
+    /**
+     * returns all values stored in this array
+     * @return map associating an index with the value stored at that index
+     */
     public Map<Integer, String> getArrayValues() {
         return arrayValues;
     }
 
+    /**
+     * returns the associated values for the given function names
+     * @return map of function name to associated value
+     */
     public Map<String, String> getFunValues() {
         return funValues;
     }
 
+    /**
+     * sets the name of this entity
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * queries the name of this entity
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+
+    /**
+     * queries the sort of this entity
+     * @return the sort
+     */
     public Sort getSort() {
         return sort;
     }
 
+    /**
+     * sets the sort of this object/array/function
+     * @param sort the sort
+     */
     public void setSort(Sort sort) {
         this.sort = sort;
     }
 
+    /**
+     * returns the length of this array
+     * @return the length
+     */
     public int getLength() {
         return length;
     }
 
+    /**
+     * sets the length of this array
+     * @param length the length
+     */
     public void setLength(int length) {
         this.length = length;
-    }
-
-    public String getName() {
-        return name;
     }
 
     /**
@@ -138,38 +196,59 @@ public class ObjectVal {
         this.fieldvalues = fieldvalues;
     }
 
+    /**
+     * returns the currently associated valut to the specified field
+     * @param field the field name
+     * @return the value associated to the field (or null if no value is known)
+     */
     public String get(String field) {
         // System.out.println(fieldvalues.keySet());
         return fieldvalues.get(field);
     }
 
+    /**
+     * associated a field to the given value
+     * @param field the field
+     * @param value the value assigned to the specified field
+     * @return the old value of the field (or null if no value was known before)
+     */
     public String put(String field, String value) {
         return fieldvalues.put(field, value);
     }
 
+    /**
+     * the value of the field specified by its short name
+     * @param name the short name of the field
+     * @return the value associated to the field (or null if not known)
+     */
     public String getFieldUsingSimpleName(String name) {
-
         if (fieldvalues.containsKey(name)) {
             return fieldvalues.get(name);
         } else {
-            for (String field : fieldvalues.keySet()) {
-                if (field.endsWith(name) || field.endsWith(
-                        name + "|")) { return fieldvalues.get(field); }
+            for (final String field : fieldvalues.keySet()) {
+                if (field.endsWith(name) ||
+                        field.endsWith(name + "|")) {
+                    return fieldvalues.get(field);
+                }
             }
         }
-
         return null;
     }
 
+    @Override
+    /**
+     * textual representation of this object
+     * @return the string representation of this object
+     */
     public String toString() {
-        String tab = "   ";
+        final String tab = "   ";
 
         // for null we don't care about length, type, etc.
-        if (name.startsWith("#o0")) { 
-            return tab + "Object " + name + "\n"; 
+        if (name.startsWith("#o0")) {
+            return tab + "Object " + name + "\n";
         }
 
-        String type = sort == null ? "java.lang.Object"
+        final String type = sort == null ? "java.lang.Object"
                 : sort.name().toString();
 
         String result = tab + "Object " + name + "\n";
@@ -179,25 +258,25 @@ public class ObjectVal {
         result += tab + tab + "exactInstance =" + this.exactInstance + "\n";
         // result += tab+tab+"<inv> = "+this.inv+"\n";
 
-        List<String> fields = new LinkedList<String>();
+        final List<String> fields = new LinkedList<String>();
         fields.addAll(fieldvalues.keySet());
         Collections.sort(fields);
 
-        for (String field : fields) {
+        for (final String field : fields) {
             result += tab + tab + field + " = " + fieldvalues.get(field);
             result += "\n";
         }
 
-        for (String fun : funValues.keySet()) {
+        for (final String fun : funValues.keySet()) {
             result += tab + tab + fun + " = " + funValues.get(fun);
             result += "\n";
         }
 
-        List<Integer> arrfields = new ArrayList<Integer>();
+        final List<Integer> arrfields = new ArrayList<Integer>();
         arrfields.addAll(arrayValues.keySet());
         Collections.sort(arrfields);
 
-        for (int i : arrfields) {
+        for (final int i : arrfields) {
             result += tab + tab + "[" + i + "] = " + arrayValues.get(i);
             result += "\n";
         }
@@ -206,22 +285,34 @@ public class ObjectVal {
 
     /**
      * Objects with equal names are equal.
+     * @param o the Object to be compared to
+     * @return true if this object and o have
+     *   equal names
      */
+    @Override
     public boolean equals(Object o) {
         if (o instanceof ObjectVal) {
-            ObjectVal ov = (ObjectVal) o;
-            if (ov.name == null) { 
-                return name == null; 
+            final ObjectVal ov = (ObjectVal) o;
+            if (ov.name == null) {
+                return name == null;
             }
             return ov.name.equals(name);
         }
         return false;
     }
 
+    /**
+     * sets a set of values stored at the indexes of this arrays
+     * @param newArrayValues the map associated an array element to its value
+     */
     public void setArrayValues(Map<Integer, String> newArrayValues) {
         this.arrayValues = newArrayValues;
     }
 
+    /**
+     * sets a set of values to be associated with the given function names
+     * @param newArrayValues the map associated function names to their respective value
+     */
     public void setFunValues(Map<String, String> newFunValues) {
         this.funValues = newFunValues;
     }
