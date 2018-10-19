eb02741bcd1df9b6fc585f0e4fd43e64d6a2917d
Jonas Schiffl <uvdes@student.kit.edu>
2018-10-19 16:52:15 +0200

    added ledger data spec

--- key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/LedgerData.java
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/LedgerData.java
@@ -3,8 +3,18 @@ package org.hyperledger.fabric.shim;
 /** superclass of all objects that can be stored on the ledger */
 public abstract class LedgerData {
 
+    /*@ public normal_behaviour
+      ensures \dl_array2seq(\result) == \dl_serializeLedgerData(\dl_object2LedgerData(g));
+      ensures \fresh(\result);
+      assignable \nothing;                
+      @*/
     public abstract byte[] serialize(LedgerData ld);
 
+    /*@ public normal_behaviour
+      ensures \dl_object2LedgerData(\result) == \dl_deserializeLedgerData(\dl_array2seq(bytes));
+      ensures \fresh(\result);
+      assignable \nothing;                
+      @*/
     public abstract LedgerData deserialize(byte[] b);
 
 }
