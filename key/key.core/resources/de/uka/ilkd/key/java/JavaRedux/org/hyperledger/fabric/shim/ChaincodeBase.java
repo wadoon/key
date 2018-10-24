26ffa2c734252b8d104035ebd50eab50198c066d
Michael Kirsten <kirsten@kit.edu>
2018-10-24 18:45:34 +0200

    Added ClientIdentity to JavaRedux

--- key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/ChaincodeBase.java
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/ChaincodeBase.java
@@ -11,43 +11,40 @@ import org.hyperledger.fabric.shim.Chaincode.Response.Status;
 
 public abstract class ChaincodeBase implements Chaincode {
 
-	
-	public abstract Response init(ChaincodeStub stub);
+    public abstract Response init(ChaincodeStub stub);
 
-	
-	public abstract Response invoke(ChaincodeStub stub);
+    public abstract Response invoke(ChaincodeStub stub);
 
-	protected static Response newSuccessResponse(String message, byte[] payload) {
-		return new Response(Status.SUCCESS, message, payload);
-	}
+    protected static Response newSuccessResponse(String message, byte[] payload) {
+        return new Response(Status.SUCCESS, message, payload);
+    }
 
-	protected static Response newSuccessResponse() {
-		return newSuccessResponse(null, null);
-	}
+    protected static Response newSuccessResponse() {
+        return newSuccessResponse(null, null);
+    }
 
-	protected static Response newSuccessResponse(String message) {
-		return newSuccessResponse(message, null);
-	}
+    protected static Response newSuccessResponse(String message) {
+        return newSuccessResponse(message, null);
+    }
 
-	protected static Response newSuccessResponse(byte[] payload) {
-		return newSuccessResponse(null, payload);
-	}
+    protected static Response newSuccessResponse(byte[] payload) {
+        return newSuccessResponse(null, payload);
+    }
 
-	protected static Response newErrorResponse(String message, byte[] payload) {
-		return new Response(Status.INTERNAL_SERVER_ERROR, message, payload);
-	}
+    protected static Response newErrorResponse(String message, byte[] payload) {
+        return new Response(Status.INTERNAL_SERVER_ERROR, message, payload);
+    }
 
-	protected static Response newErrorResponse() {
-		return newErrorResponse(null, null);
-	}
+    protected static Response newErrorResponse() {
+        return newErrorResponse(null, null);
+    }
 
-	protected static Response newErrorResponse(String message) {
-		return newErrorResponse(message, null);
-	}
+    protected static Response newErrorResponse(String message) {
+        return newErrorResponse(message, null);
+    }
 
-	protected static Response newErrorResponse(byte[] payload) {
-		return newErrorResponse(null, payload);
-	}
+    protected static Response newErrorResponse(byte[] payload) {
+        return newErrorResponse(null, payload);
+    }
 
-	
 }
--- key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/ChaincodeStub.java
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/ChaincodeStub.java
@@ -8,35 +8,36 @@ package org.hyperledger.fabric.shim;
 
 public interface ChaincodeStub {
 
-        //@ public instance ghost \seq transactionLog;
+    //@ public instance ghost \seq transactionLog;
 
-        /*@ public normal_behaviour
-            ensures \dl_array2seq(\result) == \dl_getValue(\dl_lastEntry(transactionLog, key));
-            assignable \nothing;
-          @*/
-	byte[] /*@helper@*/ getState(int key);
+    /*@ public normal_behaviour
+      @ ensures \dl_array2seq(\result) == \dl_getValue(\dl_lastEntry(transactionLog, key));
+      @ assignable \nothing;
+      @*/
+    byte[] /*@ helper @*/ getState(int key);
 
-        /*@ public normal_behaviour
-            ensures transactionLog == \dl_seqConcat(\old(transactionLog), \dl_seqSingleton(\dl_newEntry(key,\dl_array2seq(value))));
-            assignable transactionLog;
-          @*/
-	void /*@helper@*/ putState(int key, byte[] value);
+    /*@ public normal_behaviour
+      @ ensures transactionLog
+      @   == \dl_seqConcat(\old(transactionLog),
+      @                    \dl_seqSingleton(\dl_newEntry(key,\dl_array2seq(value))));
+      @ assignable transactionLog;
+      @*/
+    void /*@ helper @*/ putState(int key, byte[] value);
 
-        /*@ public normal_behaviour
-            ensures transactionLog == \dl_seqConcat(\old(transactionLog), \dl_seqSingleton(\dl_newEntry(key,\dl_deleted)));
-            assignable transactionLog;
-          @*/
-	void /*@helper@*/ delState(int key);
+    /*@ public normal_behaviour
+      @ ensures transactionLog
+      @   == \dl_seqConcat(\old(transactionLog),
+      @                    \dl_seqSingleton(\dl_newEntry(key,\dl_deleted)));
+      @ assignable transactionLog;
+      @*/
+    void /*@ helper @*/ delState(int key);
 
-
-         /*@ public normal_behaviour
-            ensures \result.length == \dl_seqLen(\dl_filterID(transactionLog, key, 0));
-            ensures (\forall int i; 0 <= i && i < \result.length;
-                             \dl_array2seq(\result[i]) ==
-                              \dl_getValue((\dl_getEntry(\dl_filterID(transactionLog, key, 0), i)) ));
-            assignable \nothing;
-          @*/
-        byte[][] /*@helper@*/ getHistoryForKey(int key);
-
-
+    /*@ public normal_behaviour
+      @ ensures \result.length == \dl_seqLen(\dl_filterID(transactionLog, key, 0));
+      @ ensures (\forall int i; 0 <= i && i < \result.length;
+      @              \dl_array2seq(\result[i]) ==
+      @                \dl_getValue((\dl_getEntry(\dl_filterID(transactionLog, key, 0), i)) ));
+      @ assignable \nothing;
+      @*/
+    byte[][] /*@helper@*/ getHistoryForKey(int key);
 }
