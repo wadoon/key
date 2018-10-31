6b7452dc5f5a41f0b321828060d31acb16a3f3bd
Jonas Schiffl <jonas.schiffl@student.kit.edu>
2018-10-31 15:02:00 +0100

    removed game stuff from standard rules; made ledgerData non-abstract

--- key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/LedgerData.java
+++ key/key.core/resources/de/uka/ilkd/key/java/JavaRedux/org/hyperledger/fabric/shim/LedgerData.java
@@ -1,20 +1,23 @@
 package org.hyperledger.fabric.shim;
 
 /** superclass of all objects that can be stored on the ledger */
-public abstract class LedgerData {
+public class LedgerData {
 
     /*@ public normal_behaviour
       ensures \dl_array2seq(\result) == \dl_serializeLedgerData(\dl_object2LedgerData(ld));
       ensures \fresh(\result);
       assignable \nothing;                
       @*/
-    public static abstract byte[] serialize(LedgerData ld);
+    public static  byte[] serialize(LedgerData ld) {
+        return null;
+    }
 
     /*@ public normal_behaviour
       ensures \dl_object2LedgerData(\result) == \dl_deserializeLedgerData(\dl_array2seq(b));
       ensures \fresh(\result);
       assignable \nothing;                
       @*/
-    public static abstract LedgerData deserialize(byte[] b);
-
+    public static  LedgerData deserialize(byte[] b){
+        return null;
+    } 
 }
