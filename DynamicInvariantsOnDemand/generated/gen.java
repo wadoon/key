40dcfd06789a3bc0143326967d1779c3a87a52d4
Daniel Pöppelmann <poeppelmann.daniel@gmail.com>
2019-01-03 12:29:51 +0100

    cleaning

--- DynamicInvariantsOnDemand/generated/gen.java
+++
@@ -1,5 +0,0 @@
-public class foo {
-public int bar(int _x, int _y)
-{exc = null;q = 0;r = x;
-while ( _y<=r ) {     int a = 1;     int b = _y;                         while ( 2*b<=r ) {       a=2*a;       b=2*b;     }     r=r-b;     q=q+a;   }                 return  q; }
-}
