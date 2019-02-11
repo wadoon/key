a7f2c4504a06932b7b83cc3ad7fa6a5505773dc2
Andre Challier <andre.challier@stud.tu-darmstadt.de>
2019-02-11 10:22:07 +0100

    Added ConflictBased skeleton and Testbench

---
+++ key/key.core/src/de/uka/ilkd/key/strategy/conflictbasedinst/ConflictBasedIstantiation.java
@@ -0,0 +1,20 @@
+package de.uka.ilkd.key.strategy.conflictbasedinst;
+
+import java.util.Iterator;
+
+import de.uka.ilkd.key.logic.PosInOccurrence;
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.proof.Goal;
+import de.uka.ilkd.key.rule.RuleApp;
+import de.uka.ilkd.key.strategy.termgenerator.TermGenerator;
+
+public class ConflictBasedIstantiation implements TermGenerator{
+
+    @Override
+    public Iterator<Term> generate(RuleApp app, PosInOccurrence pos,
+            Goal goal) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
