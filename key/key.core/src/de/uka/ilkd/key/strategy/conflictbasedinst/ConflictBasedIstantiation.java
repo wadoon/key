d31f30fd001b72f0cc4c7cc291d7d8cc344c0802
Andre Challier <andre.challier@stud.tu-darmstadt.de>
2019-02-11 10:57:00 +0100

    Implemented singleton behavoir for ConflictBasedInstantiation

--- key/key.core/src/de/uka/ilkd/key/strategy/conflictbasedinst/ConflictBasedIstantiation.java
+++ key/key.core/src/de/uka/ilkd/key/strategy/conflictbasedinst/ConflictBasedIstantiation.java
@@ -8,13 +8,49 @@ import de.uka.ilkd.key.proof.Goal;
 import de.uka.ilkd.key.rule.RuleApp;
 import de.uka.ilkd.key.strategy.termgenerator.TermGenerator;
 
-public class ConflictBasedIstantiation implements TermGenerator{
+/**
+ * A conflict based instantiation generator for quantified terms.\
+ *
+ * @author Andre Challier <andre.challier@stud.tu-darmstadt.de>
+ *
+ */
+public class ConflictBasedIstantiation implements TermGenerator {
 
-    @Override
-    public Iterator<Term> generate(RuleApp app, PosInOccurrence pos,
-            Goal goal) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	/*
+	 * Singleton behavior
+	 */
+
+	// This objects singleton instance
+	private static ConflictBasedIstantiation instance;
+
+	// Prevent creation by other methods
+	private ConflictBasedIstantiation() {
+	}
+
+	/**
+	 * Returns the instance of this {@link ConflictBasedIstantiation} in a
+	 * singleton-way.
+	 * <p>
+	 * Returns an instance of {@link ConflictBasedIstantiation} if one has been
+	 * created. Otherwise a new instance will be created and returned.
+	 *
+	 * @return The instance of this {@link ConflictBasedIstantiation}
+	 */
+	public static ConflictBasedIstantiation getInstance() {
+		if (ConflictBasedIstantiation.instance == null) {
+			ConflictBasedIstantiation.instance = new ConflictBasedIstantiation();
+		}
+		return ConflictBasedIstantiation.instance;
+	}
+
+	/*
+	 * TermGenerator behavior
+	 */
+
+	@Override
+	public Iterator<Term> generate(RuleApp app, PosInOccurrence pos, Goal goal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
