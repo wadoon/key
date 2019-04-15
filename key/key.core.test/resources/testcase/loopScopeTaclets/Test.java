c5e43b1bd8c157b8021cff18125a9ed2877044ae
Grekain1 <Grekain1@users.noreply.github.com>
2019-04-15 15:13:29 +0200

    Large for example added

--- key/key.core.test/resources/testcase/loopScopeTaclets/Test.java
+++ key/key.core.test/resources/testcase/loopScopeTaclets/Test.java
@@ -94,88 +94,274 @@ public class TestNoLabels {
         return i;
     }
     
-    /*@ public normal_behavior
-    @ requires i >= 0;
-    @ ensures (\result == 0 || \result <= -1) && ((flag && \old(i) < 17) ==> \result == -2) ;
-    @*/
-    public static int loopScopeRuleBenchmarkWhileBig(int i, boolean flag) {
-        int a = 1; // 4 steps
-        int b = 2; // 4 steps
-        int c = 3; // 4 steps
-        a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 20 steps
-        a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 20 steps
-        
-        /*@ loop_invariant
-        @   i >= 0 && i <= \old(i);
-        @ decreases i;
-        @*/
-        while (i > 0) {
-            if (i == 17) {
-                i = 0;
-                continue; // have to prove the invariant
-            }
-            else if (i == 42) {
-                i = -1;
-                break; // have to prove the postcondition!
-            }
-            else if (i == 2048) {
-                i = -1;
-                break; // have to prove the postcondition!
-            }
-
-            i--;
-        }
-        
-        a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 60 steps
-        a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 60 steps
-
-        if (flag) {
-            i = -2;
-        }
-
-        return i;
-    }
+//    k = 0;
+//    /*@ loop_invariant
+//    @ k == (x-i)*x && i <= x && i >= n;
+//    @ decreases i;
+//    @*/
+//    for (i = x; i > n; i--) {
+//        k += x;
+//    }
+    
+//    k = 0; /*@ loop_invariant @ k == (x-i)*x && i <= x && i >= n; @ decreases i; @*/ for (i = x; i > n; i--) { k += x; }
     
     /*@ public normal_behavior
-    @ requires i >= 0;
-    @ ensures (\result == 0 || \result <= -1) && ((flag && \old(i) < 17) ==> \result == -2) ;
+    @ requires n >= 0 && i >= n;
+    @ ensures (\result == (\old(i)-n)*\old(i));
     @*/
-    public static int loopScopeRuleBenchmarkForBig(int i, boolean flag) {
-        int a = 1; // 4 steps
-        int b = 2; // 4 steps
-        int c = 3; // 4 steps
-        //a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 20 steps
-        //a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 20 steps
+    public static int loopScopeRuleBenchmarkForBig(int i, int n) {
+        int x = i;
+        int k;
         
+        // 10
+        k = 0;
         /*@ loop_invariant
-        @   i >= 0 && i <= \old(i);
+        @ k == (x-i)*x && i <= x && i >= n;
         @ decreases i;
         @*/
-        for (i = i; i > 0; i--) {
-            a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 20 steps
-            if (i == 17) {
-                i = 1;
-                //a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 20 steps
-                continue; // have to prove the invariant
-            }
-            else if (i == 42) {
-                i = -1;
-                break; // have to prove the postcondition!
-            }
-            else if (i == 2048) {
-                i = -1;
-                break; // have to prove the postcondition!
-            }
-        }
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
         
-        //a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 60 steps
-        //a=b;b=c;a=c;c=b;a=c;b=a;a=c;a=c;b=a;a=c; // 60 steps
-
-        if (flag) {
-            i = -2;
-        }
-
-        return i;
+     // 10
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        
+     // 10
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        
+     // 10
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        k = 0;
+        /*@ loop_invariant
+        @ k == (x-i)*x && i <= x && i >= n;
+        @ decreases i;
+        @*/
+        for (i = x; i > n; i--) { k += x; }
+        
+        return k;
     }
     
     /*@ public normal_behavior
@@ -183,21 +369,22 @@ public class TestNoLabels {
     @ ensures (\result == 0 || \result <= -1) && ((flag && \old(i) < 17) ==> \result == -2) ;
     @*/
     public static int loopScopeRuleBenchmarkNested(int i, boolean flag) {
-        int j = i;
+        int x = i;
+        
         /*@ loop_invariant
-        @   i >= 0 && i <= \old(i);
+        @   i >= -1 && i <= \old(i);
         @ decreases i;
         @*/
-        for (i = j; i > 0; i--) {
-            if (i == 17) {i = 1;continue; } else if (i == 42) { i = -1; break; } else if (i == 2048) { i = -1; break; } 
+        for (i = x; i > 0; i--) {
+            if (i == 17) {i = 1; continue; } else if (i == 42) { i = -1; break; } else if (i == 2048) { i = -1; break; } 
+            
             /*@ loop_invariant
-            @   i >= 0 && i <= \old(i);
-            @ decreases i;
+            @   j >= 0 && i <= \old(j);
+            @ decreases j;
             @*/
-            for (i = j; i > 0; i--) {
-                if (i == 17) {i = 1;continue; } else if (i == 42) { i = -1; break; } else if (i == 2048) { i = -1; break; } 
+            for (int j = i; j > 0; j--) {
+                if (j == 17) {i = 1; continue; } else if (j == 42) { i = -1; break; } else if (j == 2048) { i = -1; break; } 
             }
-            if (flag) {i = -2;}
         }
         if (flag) {i = -2;}
 
