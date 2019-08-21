db9156b9e8399b4b46e6beb650008062df3dbe07
Sarah Grebing <grebing@ira.uka.de>
2019-08-21 14:14:11 +0200

    Fixed test case

--- key/keyext.script/src/test/java/edu/kit/iti/formal/psdbg/interpreter/KeyMatcherDerivableTest.java
+++ key/keyext.script/src/test/java/edu/kit/iti/formal/psdbg/interpreter/KeyMatcherDerivableTest.java
@@ -1,42 +1,46 @@
 package edu.kit.iti.formal.psdbg.interpreter;
 
+import de.uka.ilkd.key.control.KeYEnvironment;
 import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.TermBuilder;
 import de.uka.ilkd.key.proof.Goal;
 import de.uka.ilkd.key.proof.Proof;
+import static edu.kit.iti.formal.psdbg.TestHelper.getFile;
 import edu.kit.iti.formal.psdbg.interpreter.data.GoalNode;
 import edu.kit.iti.formal.psdbg.interpreter.data.KeyData;
 import edu.kit.iti.formal.psdbg.interpreter.matcher.KeYMatcher;
+import org.apache.commons.cli.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by weigl on 15.07.2017.
+ *
  */
 public class KeyMatcherDerivableTest {
-    /*@Test
+
+    @Test
     public void derivable_test_1() throws Exception {
-        KeYProofFacade f = new KeYProofFacade();
+        //KeYProofFacade f = new KeYProofFacade();
         String file = getClass().getResource("derivable_test_1.key").toExternalForm().substring(5);
-        f.loadKeyFileSync(new File(file));
-        Proof proof = f.getProof();
+
+        KeYEnvironment<?> env = KeYEnvironment.load(new File(file), null, null, null);
+
+        Proof proof = env.getLoadedProof();
 
         Goal g = proof.getGoal(proof.root());
-        KeyData newKeYData = new KeyData(g, f.getEnvironment(), proof);
+        KeyData newKeYData = new KeyData(g, env, proof);
         GoalNode<KeyData> gn = new GoalNode<>(newKeYData);
-        Term termQ = new TermBuilder(f.getEnvironment().getServices().getTermFactory(),
-                f.getEnvironment().getServices()).parseTerm("q");
+        Term termQ = new TermBuilder(env.getServices().getTermFactory(),
+                env.getServices()).parseTerm("q");
         System.out.println(termQ);
         GoalNode<KeyData> a = KeYMatcher.isDerivable(proof, gn, termQ);
-
-
         System.out.println(proof);
-
         Assert.assertNotNull(a);
-
         Assert.assertEquals(1, proof.getSubtreeGoals(proof.root()).size());
     }
-    */
+
 }
