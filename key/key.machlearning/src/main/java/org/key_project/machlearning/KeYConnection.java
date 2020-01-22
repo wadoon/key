2c3f11111d8abdbac3d99581f809fb6d2452a747
Mattias Ulbrich <ulbrich@kit.edu>
2020-01-22 17:34:07 +0100

    model search and expansion as tactics

--- key/key.machlearning/src/main/java/org/key_project/machlearning/KeYConnection.java
+++ key/key.machlearning/src/main/java/org/key_project/machlearning/KeYConnection.java
@@ -55,6 +55,7 @@ public class KeYConnection {
     static {
         TACTICS.put("AUTO", new AutoTactic(""));
         TACTICS.put("AUTO_NOSPLIT", new AutoTactic("[StrategyProperty]SPLITTING_OPTIONS_KEY=SPLITTING_OFF"));
+        TACTICS.put("MODELSEARCH", new AutoTactic("[StrategyProperty]NON_LIN_ARITH_OPTIONS_KEY=NON_LIN_ARITH_COMPLETION"));
         TACTICS.put("NOTHING", (a, b, c, d) -> {});
         TACTICS.put("SMT", new SMTTactic());
         FilterTactic.registerTactics(TACTICS);
