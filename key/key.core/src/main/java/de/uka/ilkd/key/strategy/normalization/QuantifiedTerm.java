987a217d27058ced1c5ae257485d654b9ec7ac40
Andre Challier <andre.challier@stud.tu-darmstadt.de>
2020-02-03 15:12:46 +0100

    created simple normalization procedure

---
+++ key/key.core/src/main/java/de/uka/ilkd/key/strategy/normalization/QuantifiedTerm.java
@@ -0,0 +1,23 @@
+package de.uka.ilkd.key.strategy.normalization;
+
+import de.uka.ilkd.key.logic.op.QuantifiableVariable;
+import de.uka.ilkd.key.logic.op.Quantifier;
+
+public class QuantifiedTerm {
+
+    private final Quantifier quantifier;
+    private final QuantifiableVariable quantifiedVariable;
+
+    public QuantifiedTerm(Quantifier quantifier, QuantifiableVariable quantifiedVariable) {
+        this.quantifier = quantifier;
+        this.quantifiedVariable = quantifiedVariable;
+    }
+
+    public Quantifier getQuantifier() {
+        return this.quantifier;
+    }
+
+    public QuantifiableVariable getQuantifiedVariable() {
+        return this.quantifiedVariable;
+    }
+}
