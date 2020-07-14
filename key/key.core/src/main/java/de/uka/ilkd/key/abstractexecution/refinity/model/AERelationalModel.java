d0f518872f7e99e831b3ee58059f6383474f9202
Dominic Steinhöfel <steinhoefel@cs.tu-darmstadt.de>
2020-07-14 15:03:39 +0200

    Renamed schema files into something not such generic

--- key/key.core/src/main/java/de/uka/ilkd/key/abstractexecution/refinity/model/AERelationalModel.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/abstractexecution/refinity/model/AERelationalModel.java
@@ -54,7 +54,7 @@ import de.uka.ilkd.key.java.Services;
 @XmlAccessorType(value = XmlAccessType.FIELD)
 public class AERelationalModel {
     private static final String AE_MODEL_FILE_ENDING = ".aer";
-    private static final String SCHEMA_PATH = "/de/uka/ilkd/key/refinity/schema1.xsd";
+    private static final String SCHEMA_PATH = "/de/uka/ilkd/key/refinity/aerSchema1.xsd";
 
     @XmlElement(name = "programOne")
     private String programOne = "";
