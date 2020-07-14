2237ea3d559d092d14d0fd0d1ab2c71a648e5268
Dominic Steinhöfel <steinhoefel@cs.tu-darmstadt.de>
2020-07-14 17:31:30 +0200

    Moved file, added initial empty inst checker class w/ todos

---
+++ key/key.core/src/main/java/de/uka/ilkd/key/abstractexecution/refinity/keybridge/InstantiationChecker.java
@@ -0,0 +1,32 @@
+// This file is part of KeY - Integrated Deductive Software Design
+//
+// Copyright (C) 2001-2010 Universitaet Karlsruhe (TH), Germany
+//                         Universitaet Koblenz-Landau, Germany
+//                         Chalmers University of Technology, Sweden
+// Copyright (C) 2011-2020 Karlsruhe Institute of Technology, Germany
+//                         Technical University Darmstadt, Germany
+//                         Chalmers University of Technology, Sweden
+//
+// The KeY system is protected by the GNU General
+// Public License. See LICENSE.TXT for details.
+//
+package de.uka.ilkd.key.abstractexecution.refinity.keybridge;
+
+/**
+ * @author Dominic Steinhoefel
+ */
+public class InstantiationChecker {
+
+    public InstantiationChecker() {
+
+    }
+    
+    // TODO:
+    // 1) Parse abstract program
+    // 2) Get list of APEs
+    // 3) Create proof obligations for APE insts (first, frame & footprint)
+    //    (need symbol declarations of original model)
+    //
+    // APE inst: number of occ. -> program
+
+}
--- key/key.core/src/main/java/de/uka/ilkd/key/abstractexecution/refinity/keybridge/RelationalProofBundleConverter.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/abstractexecution/refinity/keybridge/RelationalProofBundleConverter.java
@@ -10,7 +10,7 @@
 // The KeY system is protected by the GNU General
 // Public License. See LICENSE.TXT for details.
 //
-package de.uka.ilkd.key.abstractexecution.refinity.conversion;
+package de.uka.ilkd.key.abstractexecution.refinity.keybridge;
 
 import java.io.File;
 import java.io.FileInputStream;
