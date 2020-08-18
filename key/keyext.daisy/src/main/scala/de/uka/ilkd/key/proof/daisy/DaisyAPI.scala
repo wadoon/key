9c424b0f19a2dc018fab6b6db2088d97eeb88972
rosaabbasi <rosaabbasi@mpi-sws.org>
2020-08-18 13:40:59 +0200

    compute Interval using Daisy added for  simple exprs

--- key/keyext.daisy/src/main/scala/de/uka/ilkd/key/proof/daisy/DaisyAPI.scala
+++ key/keyext.daisy/src/main/scala/de/uka/ilkd/key/proof/daisy/DaisyAPI.scala
@@ -3,23 +3,19 @@ package de.uka.ilkd.key.proof.daisy
 import java.util
 
 import daisy.analysis.DataflowPhase
-import daisy.lang.Identifiers.FreshIdentifier.{decode, uniqueCounter}
 import daisy.lang.Identifiers.{FreshIdentifier, Identifier}
 import daisy.lang.Trees.{BooleanLiteral, Division, Expr, Minus, Plus, RealLiteral, Times, Variable}
 import daisy.lang.Types
 import daisy.lang.Types.RealType
-import daisy.tools.Rational.{double2Fraction, fromReal}
 import daisy.tools.{Interval, Rational}
 import de.uka.ilkd.key.java.Services
 import de.uka.ilkd.key.java.expression.literal.FloatLiteral
 import de.uka.ilkd.key.ldt.FloatLDT
 import de.uka.ilkd.key.logic.op.Operator
-import de.uka.ilkd.key.logic.{Name, ProgramElementName, Term}
+import de.uka.ilkd.key.logic.{ProgramElementName, Term}
 import de.uka.ilkd.key.util.Pair
 import org.key_project.util.ExtList
-
 import scala.collection.JavaConverters._
-import scala.collection.mutable
 
 
 object DaisyAPI {
@@ -28,27 +24,31 @@ object DaisyAPI {
   def computeRange(preconditions: util.List[Term], floatExpr: Term, lets: util.List[Term], services: Services): Pair[java.lang.Float, java.lang.Float] = {
 
     DataflowPhase.rangeMethod = "interval"
-    val temp = merge(lets, floatExpr, services)
 
-    val range: Interval = DataflowPhase.computeRange(convertPreCondsToInputIntervals(preconditions, services),
-      merge(lets, floatExpr, services), BooleanLiteral(true)) _1
+    val daisyInputValMap = convertPreCondsToInputIntervals(preconditions, services)
+    val daisyExpression = merge(lets, floatExpr, daisyInputValMap.keys.toList, services)
+
+    val range: Interval = DataflowPhase.computeRange(daisyInputValMap, daisyExpression, BooleanLiteral(true)) _1
 
     new Pair[java.lang.Float, java.lang.Float](range.xlo.toFloat, range.xhi.toFloat)
   }
 
   def convertPreCondsToInputIntervals(preconditions: util.List[Term], services: Services): Map[Identifier, Interval] = {
 
-    var preConds = Map[Identifier, Interval]()
-    var preCondsTemp = Map[String, List[Rational]]()
+    var daisyInputValMap = Map[Identifier, Interval]()
+    var preConds = Map[String, (Option[Rational], Option[Rational])]()
+    val floatLDT = new FloatLDT(services)
 
-    def updateMap(key: String, fl: Rational): Unit = {
+    def updatePreCondsMap(key: String, fl: Rational): Unit = {
 
-      if (!preCondsTemp.isDefinedAt(key))
-        preCondsTemp += key -> List(fl)
+      if (!preConds.isDefinedAt(key))
+        preConds += key -> (Some(fl), None)
       else {
-        var item = preCondsTemp(key)
-        var newItem = fl :: item
-        preCondsTemp += key -> (newItem)
+        val intervalBounds = preConds(key)
+        if (intervalBounds._1.get < fl)
+          preConds += key -> (intervalBounds._1, Some(fl))
+        else
+          preConds += key -> (Some(fl), intervalBounds._1)
       }
     }
 
@@ -58,48 +58,52 @@ object DaisyAPI {
       val varName = termDetails.asScala.filter(p => !p.asInstanceOf[Term].isRigid).toList.head.asInstanceOf[Term].op().name().asInstanceOf[ProgramElementName].getProgramName
       val literal = termDetails.asScala.filter(p => p.asInstanceOf[Term].isRigid).toList.head.asInstanceOf[Term]
 
-      val floatLDT = new FloatLDT(services)
       val fl: Double = floatLDT.translateTerm(literal, new ExtList, services).asInstanceOf[FloatLiteral].getValue.toDouble
       val fr: Rational = Rational.fromDouble(fl)
 
-      updateMap(varName, fr)
+      updatePreCondsMap(varName, fr)
     }
-    for (item <- preCondsTemp) {
 
-      val varId = FreshIdentifier(item._1, RealType, true)
-      var itemList = preCondsTemp(item._1)
-      val sortedList = itemList.sorted
-      var varInterval = Interval(sortedList.head, sortedList(1))
+    for (item <- preConds) {
 
-      preConds += varId -> varInterval
+      val varId = FreshIdentifier(item._1, RealType, false)
+      val varLowerBound = item._2._1
+      val varUpperBound = item._2._2
+      if (varUpperBound isDefined)
+        daisyInputValMap += varId -> Interval(varLowerBound.get, varUpperBound.get)
+      else throw new Exception("Upper or lower bound not given")
+
     }
-    preConds
+
+    daisyInputValMap
   }
 
-  def merge(lets: util.List[Term], floatExpr: Term, services: Services): Expr = {
+  //TODO  add support for let expressions
+  def merge(lets: util.List[Term], floatExpr: Term, varIds: List[Identifier], services: Services): Expr = {
 
-    val op: Operator = floatExpr.op()
     val floatLDT = new FloatLDT(services)
-    val subterms = floatExpr.subs()
+    val op: Operator = floatExpr.op()
+    val subTerms = floatExpr.subs()
 
     if (op == floatLDT.getJavaAdd)
-      Plus(merge(lets, subterms.get(0), services), merge(lets, subterms.get(1), services))
+      Plus(merge(lets, subTerms.get(0), varIds, services), merge(lets, subTerms.get(1), varIds, services))
     else if (op == floatLDT.getJavaSub)
-      Minus(merge(lets, subterms.get(0), services), merge(lets, subterms.get(1), services))
+      Minus(merge(lets, subTerms.get(0), varIds, services), merge(lets, subTerms.get(1), varIds, services))
     else if (op == floatLDT.getJavaMul)
-      Times(merge(lets, subterms.get(0), services), merge(lets, subterms.get(1), services))
+      Times(merge(lets, subTerms.get(0), varIds, services), merge(lets, subTerms.get(1), varIds, services))
     else if (op == floatLDT.getJavaDiv)
-      Division(merge(lets, subterms.get(0), services), merge(lets, subterms.get(1), services))
+      Division(merge(lets, subTerms.get(0), varIds, services), merge(lets, subTerms.get(1), varIds, services))
+    // if variable
     else if (!op.isRigid && op.arity() == 0) {
-      val name = op.name().asInstanceOf[ProgramElementName].getProgramName
-      Variable(FreshIdentifier(name, RealType, true))
+      val varName = op.name().asInstanceOf[ProgramElementName].getProgramName
+      Variable(varIds.filter(id => id.name == varName).head)
     }
+    // if literal
     else if (op.name().toString == "FP") {
       val fl: Double = floatLDT.translateTerm(floatExpr, new ExtList, services).asInstanceOf[FloatLiteral].getValue.toDouble
       RealLiteral(Rational.fromDouble(fl))
     }
-    else
-      BooleanLiteral(true)
+    else throw new Exception("Operation" + op + "not supported")
   }
 
   def main(args: Array[String]): Unit = {
