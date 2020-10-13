e6d4421031cab4fab78e08451ad40c5a50f7e2c1
Alexander Weigl <weigl@kit.edu>
2020-10-14 00:23:49 +0200

    working parser and interpreter

--- key/key.core/src/main/java/de/uka/ilkd/key/parser/njava/JavaParserFacade.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/parser/njava/JavaParserFacade.java
@@ -4,12 +4,49 @@ import de.uka.ilkd.key.nparser.JavaKLexer;
 import de.uka.ilkd.key.nparser.JavaKParser;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+import recoder.java.Comment;
+import recoder.java.CompilationUnit;
+import recoder.java.SingleLineComment;
+import recoder.list.generic.ASTArrayList;
+import recoder.list.generic.ASTList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Alexander Weigl
  * @version 1 (10/12/20)
  */
 public class JavaParserFacade {
+    public static CompilationUnit parseCompilationUnit(CharStream stream) {
+        ASTList<Comment> comments = getCommentTokens(stream);
+        JavaKParser.CompilationUnitContext unit = createParser(stream).compilationUnit();
+        JavaKBuilder builder = new JavaKBuilder();
+        CompilationUnit cu = (CompilationUnit) unit.accept(builder);
+        cu.setComments(comments);
+        return cu;
+    }
+
+    private static ASTList<Comment> getCommentTokens(CharStream stream) {
+        ASTList<Comment> seq = new ASTArrayList<>();
+        JavaKLexer lexer = createLexer(stream);
+        CommonTokenStream commentStream = new CommonTokenStream(lexer, JavaKLexer.HIDDEN);
+        Token token = commentStream.get(1);
+        do {
+            if (token.getType() == JavaKLexer.LINE_COMMENT){
+                Comment c = new SingleLineComment(token.getText());
+                seq.add(c);
+            }
+            if(token.getType() == JavaKLexer.COMMENT) {
+                Comment c = new Comment(token.getText());
+                seq.add(c);
+            }
+            commentStream.consume();
+        } while (token.getType() != Token.EOF);
+        return seq;
+    }
+
     public static  JavaKParser.CompilationUnitContext parseFile(CharStream stream) {
         return createParser(stream).compilationUnit();
     }
