d16d39e6133e3181d2607191dd1f0e6b0494cf8b
Alexander Weigl <weigl@kit.edu>
2020-10-15 22:32:09 +0200

    try to print via r2k

--- key/key.core/src/test/java/de/uka/ilkd/key/parser/njava/RegressionTest.java
+++ key/key.core/src/test/java/de/uka/ilkd/key/parser/njava/RegressionTest.java
@@ -1,6 +1,15 @@
 package de.uka.ilkd.key.parser.njava;
 
+import de.uka.ilkd.key.java.ProgramElement;
+import de.uka.ilkd.key.java.Recoder2KeY;
+import de.uka.ilkd.key.java.Recoder2KeYConverter;
+import de.uka.ilkd.key.java.Services;
 import de.uka.ilkd.key.java.recoderext.ProofJavaProgramFactory;
+import de.uka.ilkd.key.logic.ProgramElementName;
+import de.uka.ilkd.key.pp.LogicPrinter;
+import de.uka.ilkd.key.pp.NotationInfo;
+import de.uka.ilkd.key.pp.ProgramPrinter;
+import de.uka.ilkd.key.proof.init.JavaProfile;
 import org.antlr.v4.runtime.CharStreams;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +17,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import recoder.ParserException;
 import recoder.java.CompilationUnit;
+import recoder.java.JavaProgramElement;
 import recoder.java.PrettyPrinter;
 import recoder.java.SourceElement;
 
@@ -20,16 +30,27 @@ import java.util.*;
  */
 @RunWith(Parameterized.class)
 public class RegressionTest {
-    private static Properties p = new Properties();
-
+    static FileWriter fwActual;
+    static FileWriter fwExpected;
     static {
-        p.setProperty("indentationAmount", "4");
-        p.setProperty("overwriteIndentation", "true");
-        p.setProperty("overwriteParsePositions", "true");
-        p.setProperty("glueParameterLists", "false");
-        p.setProperty("glueParameters", "false");
-        p.setProperty("glueControlExpressions", "false");
-        p.setProperty("glueExpressionParentheses", "false");
+        try {
+            fwActual = new FileWriter("/tmp/a.txt");
+            fwExpected = new FileWriter("/tmp/b.txt");
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    fwExpected.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    fwActual.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Parameterized.Parameters
@@ -37,7 +58,7 @@ public class RegressionTest {
         return readFile("/java.txt");
     }
 
-    static Collection<Object[]> readFile(String s) {
+    public static Collection<Object[]> readFile(String s) {
         InputStream in = RegressionTest.class.getResourceAsStream(s);
         HashSet<String> got = new HashSet<>();
         Collection<Object[]> seq = new ArrayList<>();
@@ -103,17 +124,26 @@ public class RegressionTest {
             default:
                 //TODO
         }
-        if (!Objects.equals(expected, actual)) {
-            /*PrettyPrinter pp = new PP(new OutputStreamWriter(System.out), p);
-            expected.accept(pp);
-            actual.accept(pp);*/
+            Services services = new Services(new JavaProfile());
+            Recoder2KeY rec2key = new Recoder2KeY(services, services.getNamespaces());
+            Recoder2KeYConverter r2k = new Recoder2KeYConverter(rec2key, services, services.getNamespaces());
+            ProgramElement keyExpected = r2k.convert((JavaProgramElement)expected);
+
+            fwExpected.write("===\n");
+            new ProgramPrinter(fwExpected).printProgramElementName((ProgramElementName) keyExpected);
+            fwExpected.write("===\n");
+
+            ProgramElement keyActual = r2k.convert((JavaProgramElement)actual);
+            fwActual.write("===\n");
+            ProgramPrinter p = new ProgramPrinter(fwActual);
+            p.printProgramElementName((ProgramElementName) keyActual);
+            fwActual.write("===\n");
+
+            //PrettyPrinter pp = new PP(new OutputStreamWriter(System.out), p);
+            //expected.accept(pp);
+            //actual.accept(pp);
+        if (!Objects.equals(fwExpected, fwActual)) {
             Assert.fail();
         }
-    }
-}
-
-class PP extends PrettyPrinter {
-    public PP(Writer out, Properties props) {
-        super(out, props);
     }
 }
