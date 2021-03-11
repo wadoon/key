715d4084efe6ab1a9e98069269c4b990866be155
Mattias Ulbrich <ulbrich@kit.edu>
2021-03-11 18:00:55 +0100

    simplifying termbuilder

--- key/key.core/src/main/java/de/uka/ilkd/key/logic/TermBuilder.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/logic/TermBuilder.java
@@ -1214,38 +1214,10 @@ public class TermBuilder {
 
         int digit;
         for (int i = j, sz = numberString.length(); i < sz; i++) {
-            switch (numberString.charAt(i)) {
-            case '0':
-                digit = 0;
-                break;
-            case '1':
-                digit = 1;
-                break;
-            case '2':
-                digit = 2;
-                break;
-            case '3':
-                digit = 3;
-                break;
-            case '4':
-                digit = 4;
-                break;
-            case '5':
-                digit = 5;
-                break;
-            case '6':
-                digit = 6;
-                break;
-            case '7':
-                digit = 7;
-                break;
-            case '8':
-                digit = 8;
-                break;
-            case '9':
-                digit = 9;
-                break;
-            default:
+            char c = numberString.charAt(i);
+            if ('0' <= c && c <= '9') {
+                digit = c - '0';
+            } else {
                 throw new NumberFormatException(numberString + " is not a number.");
             }
             numberLiteralTerm = func(intLDT.getNumberLiteralFor(digit), numberLiteralTerm);
