835f2d193f1d3f7eea40e76f94736fe267d629a0
lonnerfors <niklas_lonnerfors@yahoo.se>
2021-04-15 11:18:09 +0200

    balance member added (not complete, probably not correct either)

--- key/key.core/src/main/resources/de/uka/ilkd/key/java/JavaRedux/java/lang/Address.java
+++ key/key.core/src/main/resources/de/uka/ilkd/key/java/JavaRedux/java/lang/Address.java
@@ -3,6 +3,8 @@ package java.lang;
 public class Address {
 	
 	public static final int now; // hack
+
+    public int balance;
 	
 	public void transfer(Message msg,int amount) {}	
 	public void require(Message msg,boolean b) {}
