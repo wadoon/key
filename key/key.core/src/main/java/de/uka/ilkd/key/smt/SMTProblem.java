0fdfa6e952a33d0cc379cef55c6cf16300644a44
Samuel Teuber <samuel@samweb.org>
2021-05-19 16:20:11 +0200

    Reverted changes to SMT problem (made it private again)

--- key/key.core/src/main/java/de/uka/ilkd/key/smt/SMTProblem.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/smt/SMTProblem.java
@@ -147,7 +147,7 @@ public class SMTProblem {
                 solvers.add(solver);
         }
         
-        public static Term sequentToTerm(Sequent s, Services services) {
+        private static Term sequentToTerm(Sequent s, Services services) {
 
             ImmutableList<Term> ante = ImmutableSLList.nil();
 
