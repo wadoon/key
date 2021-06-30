cf9ef9b9cb1ab6d141b5ee013fb8176370e73baf
Samuel Teuber <samuel@samweb.org>
2021-06-30 11:18:13 +0200

    Improved resolution behavior (more powerful semantics blasting and Automode instead of Autopilot. Beware: There is still an issue with the precondition extraction in combination with Automode which sometimes produces useless subterms in the precondition

--- key/keyext.extract_preconditions/src/main/java/keyext/extract_preconditions/ExtractorTest.java
+++ key/keyext.extract_preconditions/src/main/java/keyext/extract_preconditions/ExtractorTest.java
@@ -5,9 +5,13 @@ import de.uka.ilkd.key.control.UserInterfaceControl;
 import de.uka.ilkd.key.gui.MainWindow;
 import de.uka.ilkd.key.java.abstraction.KeYJavaType;
 import de.uka.ilkd.key.logic.Name;
+import de.uka.ilkd.key.logic.ProgramElementName;
 import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.op.IObserverFunction;
+import de.uka.ilkd.key.logic.op.LocationVariable;
+import de.uka.ilkd.key.logic.op.Operator;
 import de.uka.ilkd.key.logic.op.ProgramVariable;
+import de.uka.ilkd.key.logic.sort.Sort;
 import de.uka.ilkd.key.macros.AbstractProofMacro;
 import de.uka.ilkd.key.macros.FullAutoPilotProofMacro;
 import de.uka.ilkd.key.pp.LogicPrinter;
@@ -39,7 +43,7 @@ import java.util.*;
  * @author steuber
  */
 public class ExtractorTest {
-    private static final int MAX_STEPS = 10000;
+    private static final int MAX_STEPS = 50000;
 
     /**
      * Main method allowing the test of the precondition extraction funtionality
@@ -68,8 +72,22 @@ public class ExtractorTest {
             e.printStackTrace();
             return;
         }
+        ImmutableList<ProgramVariable> projectionVars = null;
+        if (args.length>=2) {
+            Iterator<String> argsIterator = Arrays.stream(args).iterator();
+            // Skip file name
+            argsIterator.next();
+            projectionVars = ImmutableSLList.nil();
+            while (argsIterator.hasNext()) {
+                projectionVars = projectionVars.append(
+                    new LocationVariable(new ProgramElementName(argsIterator.next()), Sort.ANY)
+                );
+            }
+        }
 
-        List<UnclosedProof> unclosedProofs = generateProofs(env);
+        List<UnclosedProof> unclosedProofs = generateProofs(env,
+                                                            location.getPath().endsWith("proof"),
+                                                            projectionVars);
 
         boolean openWindow = getYesNo("Inspect proofs in KeY? (y/n)");
 
@@ -167,71 +185,92 @@ public class ExtractorTest {
      * @param env KeY envirionment
      * @return List of all proofs with open goals
      */
-    private static List<UnclosedProof> generateProofs(KeYEnvironment<?> env) {
+    private static List<UnclosedProof> generateProofs(KeYEnvironment<?> env,
+                                                      boolean loadProofFile,
+                                                      ImmutableList<ProgramVariable> projectionVariablesList) {
         // TODO(steuber): We probably want to adjust this a little? Or make it configurable...
         List<UnclosedProof> unclosedProofs = new LinkedList<>();
-        // List all specifications of all types in the source location (not classPaths and bootClassPath)
-        final List<Contract> proofContracts = new LinkedList<>();
-        Set<KeYJavaType> kjts = env.getJavaInfo().getAllKeYJavaTypes();
-        for (KeYJavaType type : kjts) {
-            if (!KeYTypeUtil.isLibraryClass(type)) {
-                ImmutableSet<IObserverFunction> targets =
-                    env.getSpecificationRepository().getContractTargets(type);
-                for (IObserverFunction target : targets) {
-                    ImmutableSet<Contract> contracts =
-                        env.getSpecificationRepository().getContracts(type, target);
-                    for (Contract contract : contracts) {
-                        proofContracts.add(contract);
+        if (loadProofFile) {
+            Proof curProof = env.getLoadedProof();
+            if (!curProof.openGoals().isEmpty()) {
+                unclosedProofs.add(
+                    new UnclosedProof(curProof, projectionVariablesList)
+                );
+            }
+            return unclosedProofs;
+        } else {
+            // List all specifications of all types in the source location (not classPaths and bootClassPath)
+            final List<Contract> proofContracts = new LinkedList<>();
+            Set<KeYJavaType> kjts = env.getJavaInfo().getAllKeYJavaTypes();
+            for (KeYJavaType type : kjts) {
+                if (!KeYTypeUtil.isLibraryClass(type)) {
+                    ImmutableSet<IObserverFunction> targets =
+                        env.getSpecificationRepository().getContractTargets(type);
+                    for (IObserverFunction target : targets) {
+                        ImmutableSet<Contract> contracts =
+                            env.getSpecificationRepository().getContracts(type, target);
+                        for (Contract contract : contracts) {
+                            proofContracts.add(contract);
+                        }
                     }
                 }
             }
-        }
-        // Perform proofs
-        for (Contract contract : proofContracts) {
-            Proof proof = null;
-            try {
-                // Create proof
-                proof =
-                    env.createProof(contract.createProofObl(env.getInitConfig(), contract));
-                // Set proof strategy options
-                StrategyProperties sp =
-                    proof.getSettings().getStrategySettings().getActiveStrategyProperties();
-                sp.setProperty(StrategyProperties.METHOD_OPTIONS_KEY,
-                    StrategyProperties.METHOD_CONTRACT);
-                sp.setProperty(StrategyProperties.DEP_OPTIONS_KEY,
-                    StrategyProperties.DEP_ON);
-                sp.setProperty(StrategyProperties.QUERY_OPTIONS_KEY,
-                    StrategyProperties.QUERY_ON);
-                sp.setProperty(StrategyProperties.NON_LIN_ARITH_OPTIONS_KEY,
-                    StrategyProperties.NON_LIN_ARITH_DEF_OPS);
-                sp.setProperty(StrategyProperties.STOPMODE_OPTIONS_KEY,
-                    StrategyProperties.STOPMODE_NONCLOSE);
-                proof.getSettings().getStrategySettings().setActiveStrategyProperties(sp);
-                // Make sure that the new options are used
-                int maxSteps = MAX_STEPS;
-                ProofSettings.DEFAULT_SETTINGS.getStrategySettings().setMaxSteps(maxSteps);
-                ProofSettings.DEFAULT_SETTINGS.getStrategySettings()
-                    .setActiveStrategyProperties(sp);
-                proof.getSettings().getStrategySettings().setMaxSteps(maxSteps);
-                AbstractProofMacro autopilot = new FullAutoPilotProofMacro();
-                autopilot.applyTo(env.getUi(), proof.root(), null, null);
-                // Show proof result
-                boolean closed = proof.openGoals().isEmpty();
-                if (!closed) {
-                    unclosedProofs.add(
-                        new UnclosedProof(proof, contract.getOrigVars().params)
-                    );
-                }
-            } catch (ProofInputException e) {
-                System.out.println("Exception at '" + contract.getDisplayName() + "' of " +
-                    contract.getTarget() + ":");
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (proof != null && proof.openGoals().isEmpty()) {
-                    proof
-                        .dispose(); // Ensure always that all instances of Proof are disposed
+            // Perform proofs
+            for (Contract contract : proofContracts) {
+                Proof proof = null;
+                try {
+                    // Create proof
+                    proof =
+                        env.createProof(contract.createProofObl(env.getInitConfig(), contract));
+                    // Set proof strategy options
+                    StrategyProperties sp =
+                        proof.getSettings().getStrategySettings().getActiveStrategyProperties();
+                    sp.setProperty(StrategyProperties.METHOD_OPTIONS_KEY,
+                        StrategyProperties.METHOD_CONTRACT);
+                    sp.setProperty(StrategyProperties.DEP_OPTIONS_KEY,
+                        StrategyProperties.DEP_ON);
+                    sp.setProperty(StrategyProperties.QUERY_OPTIONS_KEY,
+                        StrategyProperties.QUERY_ON);
+                    sp.setProperty(StrategyProperties.NON_LIN_ARITH_OPTIONS_KEY,
+                        StrategyProperties.NON_LIN_ARITH_DEF_OPS);
+                    sp.setProperty(StrategyProperties.STOPMODE_OPTIONS_KEY,
+                        StrategyProperties.STOPMODE_NONCLOSE);
+                    proof.getSettings().getStrategySettings().setActiveStrategyProperties(sp);
+                    // Make sure that the new options are used
+                    int maxSteps = MAX_STEPS;
+                    ProofSettings.DEFAULT_SETTINGS.getStrategySettings().setMaxSteps(maxSteps);
+                    ProofSettings.DEFAULT_SETTINGS.getStrategySettings()
+                        .setActiveStrategyProperties(sp);
+                    proof.getSettings().getStrategySettings().setMaxSteps(maxSteps);
+                    //AbstractProofMacro autopilot = new FullAutoPilotProofMacro();
+                    //autopilot.applyTo(env.getUi(), proof.root(), null, null);
+                    proof.setActiveStrategy(proof.getServices().getProfile().getDefaultStrategyFactory().create(proof, sp));
+                    // Start auto mode
+                    env.getUi().getProofControl().startAndWaitForAutoMode(proof);
+                    // Show proof result
+                    boolean closed = proof.openGoals().isEmpty();
+                    if (!closed) {
+                        if (projectionVariablesList != null) {
+                            unclosedProofs.add(
+                                new UnclosedProof(proof, projectionVariablesList)
+                            );
+                        } else {
+                            unclosedProofs.add(
+                                new UnclosedProof(proof, contract.getOrigVars().params)
+                            );
+                        }
+                    }
+                } catch (ProofInputException e) {
+                    System.out.println("Exception at '" + contract.getDisplayName() + "' of " +
+                        contract.getTarget() + ":");
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (proof != null && proof.openGoals().isEmpty()) {
+                        proof
+                            .dispose(); // Ensure always that all instances of Proof are disposed
+                    }
                 }
             }
         }
@@ -250,8 +289,10 @@ public class ExtractorTest {
         throws ProblemLoaderException {
         // Ensure that Taclets are parsed
         if (!ProofSettings.isChoiceSettingInitialised()) {
+            System.out.println("Trying to load "+location.getAbsolutePath());
             KeYEnvironment<?>
                 env = KeYEnvironment.load(location, classPaths, bootClassPath, includes);
+
             env.dispose();
         }
         // Set Taclet options
--- key/keyext.extract_preconditions/src/main/java/keyext/extract_preconditions/macros/PreconditionSemanticsBlastingMacro.java
+++ key/keyext.extract_preconditions/src/main/java/keyext/extract_preconditions/macros/PreconditionSemanticsBlastingMacro.java
@@ -2,15 +2,74 @@ package keyext.extract_preconditions.macros;
 
 import de.uka.ilkd.key.macros.SemanticsBlastingMacro;
 import de.uka.ilkd.key.proof.Goal;
+import de.uka.ilkd.key.proof.rulefilter.RuleFilter;
+import de.uka.ilkd.key.rule.Rule;
+
+import java.util.HashSet;
 
 /**
  * Based on de.uka.ilkd.key.macros.PreconditionSemanticsBlastingMacro
  */
 public final class PreconditionSemanticsBlastingMacro extends SemanticsBlastingMacro {
 
+	private PreconditionSemanticsRuleFilter preconditionSemanticRuleFilter;
+	private PreconditionEqualityRuleFilter preconditionEqualityRuleFilter;
+
 	public PreconditionSemanticsBlastingMacro() {
 		super();
 		allowedPullOut.remove("store");
+		preconditionSemanticRuleFilter = new PreconditionSemanticsRuleFilter();
+		preconditionEqualityRuleFilter = new PreconditionEqualityRuleFilter();
+
+	}
+
+	@Override
+	protected RuleFilter getSemanticsRuleFilter () {
+		return preconditionSemanticRuleFilter;
+	}
+
+	@Override
+	protected RuleFilter getEqualityRuleFilter () {
+		return preconditionEqualityRuleFilter;
+	}
+
+	private class PreconditionSemanticsRuleFilter extends SemanticsRuleFilter {
+		protected HashSet<String> allowedRulesNames;
+
+		{
+			allowedRulesNames = new HashSet<String>(100);
+			allowedRulesNames.add("execEmpty");
+			allowedRulesNames.add("emptyModality");
+			allowedRulesNames.add("emptyModalityBoxTransaction");
+			allowedRulesNames.add("emptyModalityDiamondTransaction");
+			allowedRulesNames.add("tryEmpty");
+			allowedRulesNames.add("tryFinallyEmpty");
+			allowedRulesNames.add("blockEmpty");
+			allowedRulesNames.add("methodCallEmpty");
+			allowedRulesNames.add("emptyStatement");
+		}
+
+		@Override
+		public boolean filter(Rule rule) {
+			return allowedRulesNames.contains(rule.name().toString()) || super.filter(rule);
+		}
+	}
+
+	private class PreconditionEqualityRuleFilter extends EqualityRuleFilter {
+		private HashSet<String> allowedRulesNames;
+		{
+			allowedRulesNames = new HashSet<String>();
+			allowedRulesNames.add("minAxiom");
+			allowedRulesNames.add("Use Operation Contract");
+			allowedRulesNames.add("assignment");
+			allowedRulesNames.add("methodCallWithAssignment");
+			allowedRulesNames.add("variableDeclaration");
+		}
+
+		@Override
+		public boolean filter(Rule rule) {
+			return allowedRulesNames.contains(rule.name().toString()) || super.filter(rule);
+		}
 	}
 
 
