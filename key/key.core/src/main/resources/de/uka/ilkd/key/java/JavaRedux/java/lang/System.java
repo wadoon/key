04c5dcbaa15f2829c3680c53c568d2dd7ab49662
Florian Lanzinger <lanzinger@kit.edu>
2021-11-25 17:12:50 +0100

    Mark System.arraycopy as helper

--- key/key.core/src/main/resources/de/uka/ilkd/key/java/JavaRedux/java/lang/System.java
+++ key/key.core/src/main/resources/de/uka/ilkd/key/java/JavaRedux/java/lang/System.java
@@ -34,7 +34,7 @@ public final class System {
       @             ((int[])dest)[destPos + i] == \old(((int[])src)[srcPos + i]));
       @   assignable ((int[])dest)[destPos .. destPos + length - 1];
       @*/
-    public static native void arraycopy(/*@nullable*/ java.lang.Object src,  int  srcPos,
+    public static /*@helper@*/ native void arraycopy(/*@nullable*/ java.lang.Object src,  int  srcPos,
                                         /*@nullable*/ java.lang.Object dest, int destPos,
                                         int length);
     // This implementation has been used to verify the above contracts
@@ -78,5 +78,5 @@ public final class System {
       @ signals_only \nothing;
       @ diverges true;
       @*/
-    public static void exit(int code);
+    public static /*@helper@*/ void exit(int code);
 }
