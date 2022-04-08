e8765b9ead10781efc62aa44ca57671c990cc513
Alexander Weigl <weigl@kit.edu>
2022-04-08 12:53:38 +0200

    Add main method

--- key/key.core/src/main/java/de/uka/ilkd/key/casl/CaslFacade.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/casl/CaslFacade.java
@@ -1,22 +1,29 @@
 package de.uka.ilkd.key.casl;
 
+import de.uka.ilkd.key.casl.parser.CASLParser;
 import de.uka.ilkd.key.casl.parser.CaslParser;
 import de.uka.ilkd.key.casl.parser.CaslVisitor;
 import de.uka.ilkd.key.casl.taclet.Taclet;
 import de.uka.ilkd.key.casl.transform.Transform;
-import de.uka.ilkd.key.casl.parser.CASLParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CaslFacade {
     private static final Logger LOGGER = LoggerFactory.getLogger(CaslFacade.class);
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("TODO: help text");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        for (String arg : args) {
+            var p = Paths.get(arg);
+            var out = Paths.get(arg + ".key");
+            var res = process(p, null);
+            Files.writeString(out, res);
+        }
     }
 
     public static String process(Path source, @Nullable String problem) throws IOException, InterruptedException {
