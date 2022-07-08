4fb4e5e23846de330922d9e83d9034a064f8f947
Alexander Weigl <weigl@kit.edu>
2022-07-08 15:44:52 +0200

    print SMT solver versions

--- key/scripts/jenkins/startupClean.sh
+++ key/scripts/jenkins/startupClean.sh
@@ -5,3 +5,10 @@ git clean -x -d -f -q
 # clean the settings to start with a defined configuration
 rm -rf /var/lib/jenkins/.key/ $HOME/.key
 
+
+cvc4 --version
+mathsat -version
+princess --version
+vampire --version
+yices-smt2 --version
+z3 -version
