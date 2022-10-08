3b45948cbc29ace95934b8620970fbc43c74b981
Mike Schwörer <pubgit@mikescher.com>
2022-10-09 01:56:16 +0200

    Added TermOrigins to `self.<inv>` (pre/post/signals)

--- key/key.core/src/main/java/de/uka/ilkd/key/speclang/jml/JMLSpecExtractor.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/speclang/jml/JMLSpecExtractor.java
@@ -17,6 +17,7 @@ import de.uka.ilkd.key.logic.label.ParameterlessTermLabel;
 import de.uka.ilkd.key.logic.op.IProgramMethod;
 import de.uka.ilkd.key.logic.op.LocationVariable;
 import de.uka.ilkd.key.logic.op.ProgramVariable;
+import de.uka.ilkd.key.logic.origin.OriginRef;
 import de.uka.ilkd.key.speclang.njml.JmlFacade;
 import de.uka.ilkd.key.speclang.njml.JmlIO;
 import de.uka.ilkd.key.speclang.njml.LabeledParserRuleContext;
@@ -430,22 +431,26 @@ public final class JMLSpecExtractor implements SpecExtractor {
                 if (!pm.isConstructor()) {
                     final ParserRuleContext ctx = JmlFacade.parseExpr(invString);
                     specCase.addClause(REQUIRES, new LabeledParserRuleContext(ctx,
-                            ParameterlessTermLabel.IMPLICIT_SPECIFICATION_LABEL));
+                            ParameterlessTermLabel.IMPLICIT_SPECIFICATION_LABEL,
+                            OriginRef.REQUIRES_SELFINVARIANT));
                 } else if (addInvariant) {
                     // add static invariant to constructor's precondition
                     final ParserRuleContext ctx = JmlFacade.parseExpr(format("%s.\\inv", pm.getName()));
                     specCase.addClause(REQUIRES, new LabeledParserRuleContext(ctx,
-                            ParameterlessTermLabel.IMPLICIT_SPECIFICATION_LABEL));
+                            ParameterlessTermLabel.IMPLICIT_SPECIFICATION_LABEL,
+                            OriginRef.REQUIRES_SELFINVARIANT));
                 }
                 if (specCase.getBehavior() != Behavior.EXCEPTIONAL_BEHAVIOR) {
                     final ParserRuleContext ctx = JmlFacade.parseExpr(invString);
                     specCase.addClause(ENSURES, new LabeledParserRuleContext(ctx,
-                            ParameterlessTermLabel.IMPLICIT_SPECIFICATION_LABEL));
+                            ParameterlessTermLabel.IMPLICIT_SPECIFICATION_LABEL,
+                            OriginRef.ENSURES_SELFINVARIANT));
                 }
                 if (specCase.getBehavior() != Behavior.NORMAL_BEHAVIOR && !pm.isModel()) {
                     final ParserRuleContext ctx = JmlFacade.parseClause(format("signals (Throwable e) %s;", invString));
                     specCase.addClause(TextualJMLSpecCase.Clause.SIGNALS, new LabeledParserRuleContext(ctx,
-                            ParameterlessTermLabel.IMPLICIT_SPECIFICATION_LABEL));
+                            ParameterlessTermLabel.IMPLICIT_SPECIFICATION_LABEL,
+                            OriginRef.SIGNALS_SELFINVARIANT));
                 }
             }
 
