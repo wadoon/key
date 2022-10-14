bbd4c7b4bfbdeb927cd59a39d652152b00ecefd6
Alexander Weigl <weigl@kit.edu>
2022-10-14 12:43:26 +0200

    simpler with a shell script

--- key/scripts/tools/upload_rap_statistics.sh
+++ key/scripts/tools/upload_rap_statistics.sh
@@ -9,38 +9,47 @@ import pprint
 import requests
 
 
-HOST = "https://git.key-project.org/api/v4"
-PROJECT_ID = 58 # see https://git.key-project.org/key/key-stats/edit
-TOKEN = os.environ["DEPLOY_TOKEN"]
+HOST="https://git.key-project.org/api/v4"
+PROJECT_ID=58 # see https://git.key-project.org/key/key-stats/edit
 
-GIT_BRANCH=...
+# This is a secret given by JENKINS
+# $DEPLOY_TOKEN
 
-def upload_files(seq):
-  # API
-  # https://docs.gitlab.com/ee/api/commits.html#create-a-commit-with-multiple-files-and-actions
-  # POST /projects/:id/repository/commits
+# PROVIDED BY JENKINS
+# https://hudson.se.informatik.tu-darmstadt.de/env-vars.html/
+# $BRANCH_NAME
 
-  url = f"{HOST}/projects/{PROJECT_ID}/repository/files/{destination}"
+TARGET_FOLDER="statistics/$BRANCH_NAME/$BUILD_NUMBER"
+MESSAGE="auto-commit: Publish statistics for run $BUILD_DISPLAY_NAME in $JOB_NAME
 
-  actions = []
-  payload = {
-    'branch' : 'main',
-    'commit_message' : f"auto-commit: Run all proofs statistics from branch {GIT_BRANCH}",
-    'actions': actions
-  }
+Files are added in $TARGET_FOLDER
+Goto Jenkins: $JOB_DISPLAY_URL
+"
 
-  for destination, local_filename in seq:
-    with open(local_filename) as fh:
-        content = fh.read()
+## Write Environment information
+set > env.sh
 
-    act = {
-        'action':  'create', # any of create, delete, move, update, chmod
-        "file_path": destination,
-        "content": content,
+## Statistic files
+FUN_CSV=key.core/build/reports/runallproofs/runStatistics.csv
+IF_CSV=key.core/build/reports/runallproofs/runStatistics_infflow.csv
 
-    }
+curl --request POST \
+     --form "branch=master" \
+     --form "commit_message=$MESSAGE" \
+     --form "start_branch=master" \
+     \
+     --form "actions[][action]=create" \
+     --form "actions[][file_path]=$TARGET_FOLDER/env.sh" \
+     --form "actions[][content]=<env.sh" \
+     \
+     --form "actions[][action]=create" \
+     --form "actions[][file_path]=$TARGET_FOLDER/run_functional_stats.csv" \
+     --form "actions[][content]=<$FUN_CSV" \
+     \
+     --form "actions[][action]=create" \
+     --form "actions[][file_path]=$TARGET_FOLDER/run_infflow_stats.csv" \
+     --form "actions[][content]=<$IF_CSV" \
+     \
+     --header "PRIVATE-TOKEN: $DEPLOY_TOKEN" \
+     "$HOST/projects/$PROJECT_ID/repository/commits"
 
-  resp = requests.post(checkUrl,data=json., headers={"Private-Token": TOKEN})
-  if resp.status_code != 200:
-      print(resp.content)
-      sys.exit(0)
