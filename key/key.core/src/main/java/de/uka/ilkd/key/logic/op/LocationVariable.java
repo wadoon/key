b0856de0694061a065018038f4a35464e5d903ae
Julian Wiesler <wiesleju@gmail.com>
2022-10-19 20:44:54 +0200

    Fix some places where location variables would forget modifiers

--- key/key.core/src/main/java/de/uka/ilkd/key/logic/op/LocationVariable.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/logic/op/LocationVariable.java
@@ -50,9 +50,15 @@ public final class LocationVariable extends ProgramVariable implements Updateabl
     public UpdateableOperator rename(Name name) {
         if (getKeYJavaType() != null) {
             return new LocationVariable(new ProgramElementName(name.toString()), getKeYJavaType(),
-                getContainerType(), isStatic(), isModel());
+                getContainerType(), isStatic(), isModel(), isGhost(), isFinal());
         } else {
             return new LocationVariable(new ProgramElementName(name.toString()), sort());
         }
+    }
+
+    public static LocationVariable fromProgramVariable(ProgramVariable variable,
+            ProgramElementName name) {
+        return new LocationVariable(name, variable.getKeYJavaType(), variable.getContainerType(),
+            variable.isStatic(), variable.isModel(), variable.isGhost(), variable.isFinal());
     }
 }
