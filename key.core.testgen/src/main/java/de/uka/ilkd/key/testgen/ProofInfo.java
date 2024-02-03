e9eb1a5c0fc2274772e330cdcae5c3e8d546739c
Alexander Weigl <weigl@kit.edu>
2024-02-03 03:11:23 +0100

    It run for BinarySearch

--- key.core.testgen/src/main/java/de/uka/ilkd/key/testgen/ProofInfo.java
+++ key.core.testgen/src/main/java/de/uka/ilkd/key/testgen/ProofInfo.java
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: GPL-2.0-only */
 package de.uka.ilkd.key.testgen;
 
-import java.util.Set;
-
 import de.uka.ilkd.key.java.Services;
 import de.uka.ilkd.key.java.abstraction.KeYJavaType;
+import de.uka.ilkd.key.java.declaration.ParameterDeclaration;
+import de.uka.ilkd.key.java.declaration.VariableSpecification;
 import de.uka.ilkd.key.logic.JavaBlock;
 import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.label.TermLabelManager;
@@ -20,9 +20,12 @@ import de.uka.ilkd.key.proof.mgt.SpecificationRepository;
 import de.uka.ilkd.key.speclang.Contract;
 import de.uka.ilkd.key.speclang.Contract.OriginalVariables;
 import de.uka.ilkd.key.speclang.FunctionalOperationContract;
-
+import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+import java.util.Set;
 
 public record ProofInfo(Proof proof, Services services) {
     private static final Logger LOGGER = LoggerFactory.getLogger(ProofInfo.class);
@@ -31,6 +34,8 @@ public record ProofInfo(Proof proof, Services services) {
         this(proof, proof.getServices());
     }
 
+
+    @Nullable
     public IProgramMethod getMUT() {
         SpecificationRepository spec = services.getSpecificationRepository();
         IObserverFunction f = spec.getTargetOfProof(proof);
@@ -41,6 +46,41 @@ public record ProofInfo(Proof proof, Services services) {
         }
     }
 
+    @Nullable
+    public String getMUTCall() {
+        var m = getMUT();
+        if (m == null) return null;
+
+        var name = m.getFullName();
+        if (name == null) return null;
+
+        StringBuilder params = new StringBuilder();
+        for (ParameterDeclaration p : m.getParameters()) {
+            for (VariableSpecification v : p.getVariables()) {
+                IProgramVariable var = v.getProgramVariable();
+                params.append(",").append(var.name());
+            }
+        }
+        if (!params.isEmpty()) {
+            params = new StringBuilder(params.substring(1));
+        }
+
+        String caller;
+        if (m.isStatic()) {
+            caller = getTypeOfClassUnderTest().getName();
+        } else {
+            caller = "self";
+        }
+
+        if (m.getReturnType().equals(KeYJavaType.VOID_TYPE)) {
+            return caller + "." + name + "(" + params + ");";
+        } else {
+            String returnType = m.getReturnType().getFullName();
+            return returnType + " result = " + caller + "." + name + "(" + params + ");";
+        }
+    }
+
+    @Nullable
     public KeYJavaType getTypeOfClassUnderTest() {
         if (getMUT() == null) {
             return null;
@@ -48,13 +88,14 @@ public record ProofInfo(Proof proof, Services services) {
         return getMUT().getContainerType();
     }
 
+    @Nullable
     public KeYJavaType getReturnType() {
         return getMUT().getType();
     }
 
     public Contract getContract() {
         ContractPO po = services.getSpecificationRepository().getPOForProof(proof);
-        return po.getContract();
+        return Objects.requireNonNull(po).getContract();
     }
 
     public Term getPostCondition() {
