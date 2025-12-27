d94ff143d4b61352654389468625d4f61006f878
Alexander Weigl <weigl@kit.edu>
2025-12-27 22:27:51 +0100

    better documentation

--- keyext.api.doc/src/main/java/org/key_project/key/api/doc/DocGen.java
+++ keyext.api.doc/src/main/java/org/key_project/key/api/doc/DocGen.java
@@ -3,19 +3,20 @@
  * SPDX-License-Identifier: GPL-2.0-only */
 package org.key_project.key.api.doc;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.function.Supplier;
-
+import de.uka.ilkd.key.util.KeYConstants;
+import de.uka.ilkd.key.util.KeYResourceManager;
 import freemarker.core.HTMLOutputFormat;
 import freemarker.ext.beans.ZeroArgumentNonVoidMethodPolicy;
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * Generation of Markdown documentation.
@@ -38,6 +39,7 @@ public class DocGen implements Supplier<String> {
             var cfg = new Configuration(Configuration.VERSION_2_3_32);
             cfg.setClassForTemplateLoading(DocGen.class, "/templates"); // classpath
             cfg.setDefaultEncoding("UTF-8");
+            cfg.setAutoEscapingPolicy(Configuration.DISABLE_AUTO_ESCAPING_POLICY);
             cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
             cfg.setLogTemplateExceptions(false);
             cfg.setWrapUncheckedExceptions(true);
@@ -49,7 +51,7 @@ public class DocGen implements Supplier<String> {
             wrapper.setExposeFields(true);
             wrapper.setMethodsShadowItems(true);
             wrapper.setRecordZeroArgumentNonVoidMethodPolicy(
-                ZeroArgumentNonVoidMethodPolicy.BOTH_METHOD_AND_PROPERTY_UNLESS_BEAN_PROPERTY_READ_METHOD);
+                    ZeroArgumentNonVoidMethodPolicy.BOTH_METHOD_AND_PROPERTY_UNLESS_BEAN_PROPERTY_READ_METHOD);
             wrapper.setForceLegacyNonListCollections(false);
             wrapper.setDefaultDateType(freemarker.template.TemplateDateModel.DATETIME);
             cfg.setObjectWrapper(wrapper);
@@ -60,15 +62,25 @@ public class DocGen implements Supplier<String> {
             model.put("segmentDocumentation", metamodel.segmentDocumentation());
 
             model.put("endpoints",
-                metamodel.endpoints()
-                        .stream().sorted(Comparator.comparing(Metamodel.Endpoint::name))
-                        .toList());
+                    metamodel.endpoints()
+                            .stream().sorted(Comparator.comparing(Metamodel.Endpoint::name))
+                            .toList());
+
+            final Map<String, List<Metamodel.Endpoint>> collect = metamodel.endpoints()
+                    .stream().sorted(Comparator.comparing(Metamodel.Endpoint::name))
+                    .collect(Collectors.groupingBy(Metamodel.Endpoint::segment, TreeMap::new, Collectors.toList()));
+
+            model.put("endpointsBySegment", collect);
 
             model.put("types",
-                metamodel.types().values()
-                        .stream().sorted(Comparator.comparing(Metamodel.Type::name))
-                        .toList());
+                    metamodel.types().values()
+                            .stream().sorted(Comparator.comparing(Metamodel.Type::name))
+                            .toList());
 
+            model.put("version",
+                    KeYResourceManager.getManager().getVersion() +
+                            " (" + KeYConstants.INTERNAL_VERSION.substring(0, 8) + ")");
+            model.put("date", new Date());
 
             // 3) Get template
             var tpl = cfg.getTemplate("docs.ftl");
